5d3286fe051d00b6dabe5cc064dcbed05f1b6b64
Nick Mitchell <nickm@us.ibm.com>
2019-01-22 13:48:05 -0500

    fix(openwhisk): fix for misplaced test file
    Fixes #263

--- plugins/plugin-openwhisk/src/test/openwhisk2/no_wskprops.ts
+++ plugins/plugin-openwhisk/src/test/openwhisk2/no_wskprops.ts
@@ -19,9 +19,9 @@
  *    this test also covers toggling the sidecar
  */
 
-import * as common from '@kui/core/tests/lib/common'
-import * as ui from '@kui/core/tests/lib/ui'
-import * as openwhisk from '@kui/plugin-openwhisk/tests/lib/openwhisk/openwhisk'
+import * as common from '@kui-shell/core/tests/lib/common'
+import * as ui from '@kui-shell/core/tests/lib/ui'
+import * as openwhisk from '@kui-shell/plugin-openwhisk/tests/lib/openwhisk/openwhisk'
 const { cli } = ui
 
 describe('no .wskprops tests', function (this: common.ISuite) {
