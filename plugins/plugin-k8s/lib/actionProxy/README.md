8fbdd4fff5e1bd9bd3407192ea3095df6f970ee5
Nick Mitchell <nickm@us.ibm.com>
2019-01-29 13:31:23 -0500

    chore(k8s): remove unused openwhisk action proxy for kubectl and helm execution
    Fixes #300

--- plugins/plugin-k8s/lib/actionProxy/README.md
+++
@@ -1,3 +0,0 @@
-The Dockerfile and build.sh are not currently used. You may find them
-useful at some point in the future, if you want to deploy the action
-proxy as a container.
--- plugins/plugin-k8s/lib/actionProxy/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKER_USER=${DOCKER_USER-starpit}
-IMAGE=${DOCKER_USER}/kubectl-action
-
-echo "Building $IMAGE"
-cp ../package.json . && docker build . -t $IMAGE; rm package.json
-docker push $IMAGE
