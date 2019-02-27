abc00d860e483df69c4b7b42b46a297f4a10d724
Nick Mitchell <nickm@us.ibm.com>
2019-02-26 22:44:07 -0500

    feat(plugins/plugin-k8s): support a gamut of helm client versions in proxy
    Fixes #570

---
+++ plugins/plugin-k8s/src/lib/util/discovery/helm-client.ts
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Debug from 'debug'
+const debug = Debug('k8s/discovery/helm-client-version')
+
+import { join } from 'path'
+import { exists } from 'fs-extra'
+import { major, minor } from 'semver'
+
+/**
+ * Attempt to pick a local helm executable that is compatible with
+ * what is running on the server
+ *
+ */
+export default async (env): Promise<string> => {
+  if (process.env.KUI_HELM_CLIENTS_DIR) {
+    debug('attempting to infer a matching helm client', env.KUBECONFIG, env.PATH)
+    const { exec } = await import('child_process')
+
+    const helmLatest = process.env.HELM_LATEST_VERSION
+    const versionLine = await new Promise<string>((resolve, reject) => {
+      try {
+        exec(`${helmLatest} --short --server version`, { env }, (err, stdout, stderr) => {
+          if (err) {
+            console.error(stderr)
+            console.error(err)
+            reject(err)
+          } else {
+            resolve(stdout)
+          }
+        })
+      } catch (err) {
+        reject(err)
+      }
+    })
+    debug('versionLine', versionLine)
+
+    if (versionLine.trim().length > 0) {
+      // e.g. Server: v2.10.0+g9ad53aa
+      const version = versionLine.replace(/Server: v(\d+\.\d+)/, '$1')
+      debug('version', version)
+
+      if (version.trim().length > 0) {
+        const helmClient = `helm-${major(version)}.${minor(version)}`
+        const helmClientPath = join(process.env.KUI_HELM_CLIENTS_DIR, helmClient)
+        debug('helmClientPat', helmClientPath)
+
+        if (exists(helmClientPath)) {
+          return helmClientPath
+        }
+      }
+    }
+  } // intentional fall-through
+
+  debug('using plain "helm"')
+  return 'helm'
+}
