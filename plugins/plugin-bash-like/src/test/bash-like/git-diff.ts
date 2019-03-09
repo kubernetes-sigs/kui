492a156e13ac1cb786500ab380a2e9cd234f699b
Nick Mitchell <nickm@us.ibm.com>
2019-03-08 22:59:24 -0500

    feat: sidecar full screen
    also port git diff to use this new feature
    
    Fixes #294
    Fixes #721

---
+++ plugins/plugin-bash-like/src/test/bash-like/git-diff.ts
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as common from '@kui-shell/core/tests/lib/common'
+import * as ui from '@kui-shell/core/tests/lib/ui'
+const { cli, selectors, sidecar } = ui
+
+import { copyFile, unlink, writeFile } from 'fs'
+import { dirname, join, normalize } from 'path'
+
+/** modify the top-level README.md, so that we can exhibit a git diff */
+const modifyTopLevelReadme = () => new Promise(async (resolve, reject) => {
+  try {
+    const copy = new Promise((resolve, reject) => {
+      copyFile('../../README.md', '../../README-bak.md', err => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
+    })
+
+    await copy
+  } catch (err) {
+    reject(err)
+  }
+
+  writeFile('../../README.md', 'hello there', err => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve()
+    }
+  })
+})
+
+/** restore the top-level README.md from our backup copy */
+const restoreTopLevelReadme = () => new Promise((resolve, reject) => {
+  copyFile('../../README-bak.md', '../../README.md', err => {
+    if (err) {
+      reject(err)
+    } else {
+      unlink('../../README-bak.md', err => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
+    }
+  })
+})
+
+describe('git diff', function (this: common.ISuite) {
+  before(common.before(this))
+  after(common.after(this))
+
+  it('should show no diffs', () => cli.do('git diff package.json', this.app)
+     .then(cli.expectJustOK)
+     .then(sidecar.expectClosed)
+     .catch(common.oops(this)))
+
+  it('should show a diff fullscreen', async () => {
+    try {
+      await modifyTopLevelReadme()
+
+      await cli.do('git diff', this.app)
+        .then(res => res.app)
+        .then(sidecar.expectOpen)
+        .then(sidecar.expectFullscreen)
+
+      await restoreTopLevelReadme()
+    } catch (err) {
+      await restoreTopLevelReadme()
+      common.oops(this)(err)
+    }
+  })
+})
