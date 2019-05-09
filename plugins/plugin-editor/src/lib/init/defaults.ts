c35caf1b6a97060c8bc3a6d1751e7f419f609bbd
Nick Mitchell <nickm@us.ibm.com>
2019-05-08 20:10:33 -0400

    fix(plugins/plugin-editor): editor plugin hard-wires font size
    Fixes #1349

--- plugins/plugin-editor/src/lib/init/defaults.ts
+++ plugins/plugin-editor/src/lib/init/defaults.ts
@@ -28,7 +28,7 @@ export default options => ({
   scrollBeyondLastColumn: 2,
   // cursorStyle: 'block',
   fontFamily: 'var(--font-monospace)',
-  fontSize: document.body.classList.contains('subwindow') ? '16' : '14.4',
+  fontSize: parseInt(getComputedStyle(document.body).getPropertyValue('font-size').replace(/px$/, ''), 10) * (document.body.classList.contains('subwindow') ? 1 : 0.875),
 
   // specifics for readOnly mode
   glyphMargin: !options.readOnly, // needed for error indicators
