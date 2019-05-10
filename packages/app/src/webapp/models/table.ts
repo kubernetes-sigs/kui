3388d7ad78e41dcf8abbf590f9454eb862d0b6b5
Nick Mitchell <nickm@us.ibm.com>
2019-05-10 11:15:50 -0400

    fix: ls -l and ls -la do not display properly
    missing permissions column; also make `ls` use Light tables
    
    Fixes #1296
    Fixes #1278

--- packages/app/src/webapp/models/table.ts
+++ packages/app/src/webapp/models/table.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import * as Debug from 'debug'
 
 export class Row {
   attributes?: Cell[]
