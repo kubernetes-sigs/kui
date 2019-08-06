8322f0a3340c284093c6eff2500704cf17fc4ee9
Nick Mitchell <nickm@us.ibm.com>
2019-08-06 10:26:35 -0400

    fix(plugins/plugin-k8s): k8s fetch-file should retry
    Fixes #2243

--- plugins/plugin-k8s/src/lib/util/fetch-file.ts
+++ plugins/plugin-k8s/src/lib/util/fetch-file.ts
@@ -18,9 +18,74 @@ import * as Debug from 'debug'
 
 import expandHomeDir from '@kui-shell/core/util/home'
 import { findFile } from '@kui-shell/core/core/find-file'
+import { isHeadless, inBrowser } from '@kui-shell/core/core/capabilities'
+import { CodedError } from '@kui-shell/core/models/errors'
+
 const debug = Debug('k8s/util/fetch-file')
 
-import needle = require('needle')
+async function needle(method: 'get', url: string): Promise<{ statusCode: number; body: string }> {
+  if (inBrowser()) {
+    debug('fetch via xhr')
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest()
+      xhr.open(method, url)
+      xhr.addEventListener('error', () => {
+        console.error('error in xhr', xhr)
+        reject(xhr.response || 'Internal Error')
+      })
+      xhr.addEventListener('load', () => {
+        resolve({
+          statusCode: xhr.status,
+          body: xhr.response.response
+        })
+      })
+      xhr.send()
+    })
+  } else if (isHeadless()) {
+    debug('fetch via needle')
+    const needle = await import('needle')
+    return needle(method, url, { follow_max: 10 }).then(_ => ({ statusCode: _.statusCode, body: _.body }))
+  } else {
+    debug('fetch via electron.net')
+    const { net } = (await import('electron')).remote
+
+    return new Promise((resolve, reject) => {
+      const request = net.request({
+        method,
+        url,
+        redirect: 'follow'
+      })
+
+      request.on('response', response => {
+        const statusCode = response.statusCode
+        debug('got response', statusCode)
+
+        // we will accumulate the body chunks below
+        let body = ''
+
+        // Warning: you must register 'end' before 'data'.
+        // https://github.com/electron/electron/issues/12545
+        response.on('end', () => {
+          debug('got end')
+          if (response.statusCode < 300) {
+            resolve({ statusCode, body })
+          } else {
+            const error: CodedError = new Error(body)
+            error.statusCode = statusCode
+            reject(error)
+          }
+        })
+
+        response.on('data', chunk => {
+          debug('got chunk', chunk.toString())
+          body += chunk.toString()
+        })
+      })
+
+      request.end()
+    })
+  }
+}
 
 /**
  * Either fetch a remote file or read a local one
@@ -35,7 +100,23 @@ export const fetchFile = (url: string): Promise<Buffer[]> => {
     urls.map(async url => {
       if (url.match(/http(s)?:\/\//)) {
         debug('fetch remote', url)
-        return needle('get', url, { follow_max: 10 }).then(_ => _.body)
+        const fetchOnce = () => needle('get', url).then(_ => _.body)
+
+        const retry = (delay: number) => async (err: Error) => {
+          if (/timeout/.test(err.message) || /hang up/.test(err.message) || /hangup/.test(err.message)) {
+            debug('retrying', err)
+            await new Promise(resolve => setTimeout(resolve, delay))
+            return fetchOnce()
+          } else {
+            throw err
+          }
+        }
+
+        // fetch with three retries
+        return fetchOnce()
+          .catch(retry(500))
+          .catch(retry(1000))
+          .catch(retry(5000))
       } else {
         debug('fetch local', url)
 
