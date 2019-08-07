bc33c5700bd157e6b3055c9de080a19d42c45e26
Nick Mitchell <nickm@us.ibm.com>
2019-08-07 11:02:19 -0400

    feat: show proxy connection status in the UI
    also try to reconnect
    
    Fixes #2256

---
+++ plugins/plugin-bash-like/src/pty/ui.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Debug from 'debug'
+
+import { addTabStripeIcon, removeTabStripeIcon, tabStripeButtonExists } from '@kui-shell/core/webapp/views/top-tabs'
+
+const debug = Debug('plugins/bash-like/pty/ui')
+
+/** class designation for our offline indicator */
+const buttonDesignation = 'kui--plugin-bash-like--pty-offline-indicator'
+
+/**
+ * Are we currently presenting as offline or online?
+ *
+ */
+function presentation(): 'offline' | 'online' {
+  if (tabStripeButtonExists(buttonDesignation)) {
+    return 'offline'
+  } else {
+    return 'online'
+  }
+}
+
+/**
+ * Update the UI to inform the user that the connection to the proxy is offline
+ *
+ */
+export function setOffline() {
+  if (presentation() === 'online') {
+    debug('setOffline')
+
+    const tmp = document.createElement('span')
+    tmp.innerHTML =
+      '<svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32" aria-hidden="true"><path d="M30 28.59L3.41 2 2 3.41l6.4 6.41L7 15.77A1 1 0 0 0 8 17h4.83L11 28.85a1 1 0 0 0 .6 1.07 1.09 1.09 0 0 0 .4.08 1 1 0 0 0 .79-.39l6.68-8.73L28.59 30zm-7.47-11.72l3.26-4.26a1 1 0 0 0 .11-1A1 1 0 0 0 25 11h-4.75L22 3.22a1 1 0 0 0-.2-.85A1 1 0 0 0 21 2H11a1 1 0 0 0-1 .77l-.3 1.3z"></path></svg>'
+
+    const offlineIcon = tmp.querySelector('svg') as SVGElement
+
+    addTabStripeIcon(offlineIcon, buttonDesignation).classList.add('red-text')
+  }
+}
+
+/**
+ * Update the UI to inform the user that the connection to the proxy is online
+ *
+ */
+export function setOnline() {
+  debug('setOnline')
+  removeTabStripeIcon(buttonDesignation)
+}
---
+++ tools/travis/test/target.d/proxy.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2019 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# When testing locally on a laptop:
+#   PORT=8081 KUI_USE_HTTP=true ~/git/kui/tools/travis/test/target.d/proxy.sh
+#
+# When in travis, this is spawned by ./webpack.sh
+#
+
+set -e
+set -o pipefail
+
+if [ -n "$1" ]; then
+    ROOT=$(cd "$1" && pwd)
+else
+    ROOT=..
+fi
+
+start() {
+    PID=$(echo $$)
+    echo "$PID" > /tmp/kuiproxy.pid
+    echo "start proxy $PID"
+
+    cd "$ROOT"/kui && "$ROOT"/app/bin/www &
+    SUB=$!
+    echo $SUB > /tmp/kuiproxy_subprocess.pid
+
+    wait $SUB
+}
+
+stop() {
+    PID=$(cat /tmp/kuiproxy_subprocess.pid)
+    echo "stop proxy $PID"
+    kill $PID
+    kill $((PID+1))
+}
+
+fakecrash() {
+    echo "fakecrash proxy"
+    stop
+    sleep 10
+    start
+}
+
+trap fakecrash SIGHUP
+start
