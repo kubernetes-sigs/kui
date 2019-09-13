8eddaf2c2051ec17a8b7c7b9a6897051e1cb0108
Nick Mitchell <nickm@us.ibm.com>
2019-09-13 09:10:56 -0400

    test: unreliable get-namespaces-with-watch test
    this was probably two things: namespace deletion conflicts, and the grabFocus method in the core test utility was clicking on the prompt block, not the prompt
    
    Fixes #2757

--- plugins/plugin-k8s/src/test/k8s1/get-namespaces-with-watch.ts
+++ plugins/plugin-k8s/src/test/k8s1/get-namespaces-with-watch.ts
@@ -31,7 +31,12 @@ enum Status {
 }
 
 /** after a cli.do (res), wait for a table row with the given status */
-const waitForStatus = async function(this: common.ISuite, status: Status, res): Promise<string> {
+const waitForStatus = async function(
+  this: common.ISuite,
+  status: Status,
+  nsName: string,
+  res: AppAndCount
+): Promise<string> {
   const selector = await cli.expectOKWithCustom({
     selector: selectors.BY_NAME(nsName)
   })(res)
@@ -45,7 +50,7 @@ const waitForStatus = async function(this: common.ISuite, status: Status, res): 
 /** create namespace, and expect status eventually to be green */
 const createNS = async function(this: common.ISuite, kubectl: string) {
   it(`should create namespace from URL via ${kubectl}`, async () => {
-    const waitForOnline: (res: AppAndCount) => Promise<string> = waitForStatus.bind(this, Status.Online)
+    const waitForOnline: (res: AppAndCount) => Promise<string> = waitForStatus.bind(this, Status.Online, nsName)
 
     try {
       await waitForOnline(await cli.do(`${kubectl} create ns ${nsName}`, this.app))
@@ -59,7 +64,7 @@ const createNS = async function(this: common.ISuite, kubectl: string) {
 const deleteNS = function(this: common.ISuite, kubectl: string) {
   it(`should delete the namespace ${nsName} from URL via ${kubectl}`, async () => {
     try {
-      const waitForOffline: (res: AppAndCount) => Promise<string> = waitForStatus.bind(this, Status.Offline)
+      const waitForOffline: (res: AppAndCount) => Promise<string> = waitForStatus.bind(this, Status.Offline, nsName)
 
       const res = await cli.do(`${kubectl} delete ns ${nsName}`, this.app)
 
@@ -70,8 +75,11 @@ const deleteNS = function(this: common.ISuite, kubectl: string) {
   })
 }
 
-/** drilldown the watch table */
-const testDrilldown = async (name: string, res: AppAndCount) => {
+/**
+ * Drilldown to a given namespace from the watch table
+ *
+ */
+const testDrilldown = async (nsName: string, res: AppAndCount) => {
   const selector = await cli.expectOKWithCustom({
     selector: selectors.BY_NAME(nsName)
   })(res)
@@ -81,7 +89,7 @@ const testDrilldown = async (name: string, res: AppAndCount) => {
   await sidecar
     .expectOpen(res.app)
     .then(sidecar.expectMode(defaultModeForGet))
-    .then(sidecar.expectShowing(name))
+    .then(sidecar.expectShowing(nsName))
 
   await res.app.client.click(selectors.SIDECAR_FULLY_CLOSE_BUTTON)
   await sidecar.expectClosed(res.app)
@@ -91,20 +99,25 @@ const testDrilldown = async (name: string, res: AppAndCount) => {
 const watchNS = function(this: common.ISuite, kubectl: string) {
   const watchCmds = [`${kubectl} get ns -w`, `${kubectl} get ns ${nsName} -w`, `${kubectl} get -w ns ${nsName}`]
 
-  watchCmds.forEach(watchCmd => {
+  watchCmds.forEach((_watchCmd, idx) => {
+    const nsNameForIter = `${nsName}-${idx}`
+    const watchCmd = _watchCmd.replace(nsName, nsNameForIter)
+
+    it('should reload', () => common.refresh(this))
+
     it(`should watch namespaces via ${watchCmd}`, async () => {
       try {
-        const waitForOnline = waitForStatus.bind(this, Status.Online)
-        const waitForOffline = waitForStatus.bind(this, Status.Offline)
+        const waitForOnline = waitForStatus.bind(this, Status.Online, nsNameForIter)
+        const waitForOffline = waitForStatus.bind(this, Status.Offline, nsNameForIter)
 
-        const createBadge = await waitForOnline(await cli.do(`${kubectl} create ns ${nsName}`, this.app))
+        const createBadge = await waitForOnline(await cli.do(`${kubectl} create ns ${nsNameForIter}`, this.app))
 
         const testWatch = await cli.do(watchCmd, this.app)
         const watchBadge = await waitForOnline(testWatch)
         const watchBadgeButOffline = watchBadge.replace(Status.Online, Status.Offline)
-        await testDrilldown(nsName, testWatch)
+        await testDrilldown(nsNameForIter, testWatch)
 
-        const deleteBadge = await waitForOffline(await cli.do(`${kubectl} delete ns ${nsName}`, this.app))
+        const deleteBadge = await waitForOffline(await cli.do(`${kubectl} delete ns ${nsNameForIter}`, this.app))
 
         // the create and delete badges had better still exist
         await this.app.client.waitForExist(createBadge)
@@ -118,7 +131,7 @@ const watchNS = function(this: common.ISuite, kubectl: string) {
         await this.app.client.waitForExist(watchBadgeButOffline)
 
         // create again
-        await waitForOnline(await cli.do(`${kubectl} create ns ${nsName}`, this.app))
+        await waitForOnline(await cli.do(`${kubectl} create ns ${nsNameForIter}`, this.app))
 
         // the "online" badge from the watch had better now exist again after the create
         // (i.e. we had better actually be watching!)
@@ -128,7 +141,7 @@ const watchNS = function(this: common.ISuite, kubectl: string) {
         await this.app.client.waitForExist(watchBadgeButOffline, 20000, true)
 
         // delete again
-        await waitForOffline(await cli.do(`${kubectl} delete ns ${nsName}`, this.app))
+        await waitForOffline(await cli.do(`${kubectl} delete ns ${nsNameForIter}`, this.app))
 
         // the "online" badge from the watch had better *NOT* exist after the delete
         // (i.e. we had better actually be watching!)
