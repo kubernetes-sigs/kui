dc754c424cb827131a9c82e691437742d1850592
Nick Mitchell <nickm@us.ibm.com>
2019-10-11 18:20:24 -0400

    fix: improve error handling in bin/pack.sh
    Fixes #2979

--- bin/pack.sh
+++ bin/pack.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 
-if [ ! -d /tmp/kui-packs ]; then
-    mkdir /tmp/kui-packs
-else
-    rm -f /tmp/kui-packs/*
-fi
+rm -rf /tmp/kui-packs && mkdir /tmp/kui-packs
 
 for i in plugins/*; do
     if [ -d $i ]; then
         echo "$(tput setaf 8)packing $(basename $i)$(tput sgr0)"
-        (cd $i && npm pack >& /dev/null && mv kui-shell*.tgz /tmp/kui-packs) &
+        (cd $i && npm pack > /dev/null 2>> /tmp/kui-packs.err && mv kui-shell*.tgz /tmp/kui-packs) &
     fi
 done
 
 for i in packages/*; do
     if [ -d $i ]; then
         echo "$(tput setaf 8)packing $(basename $i)$(tput sgr0)"
-        (cd $i && npm pack >& /dev/null && mv kui-shell*.tgz /tmp/kui-packs) &
+        (cd $i && npm pack > /dev/null 2>> /tmp/kui-packs.err && mv kui-shell*.tgz /tmp/kui-packs) &
     fi
 done
 
 wait
+CODE=$?
+
+if [ $CODE != 0 ]; then
+    echo "Error generating packs. You will find the error logs in /tmp/kui-packs.err"
+    if [ -n "$TRAVIS_JOB_ID" ]; then
+        cat /tmp/kui-packs.err
+    fi
+fi
+
+if [ -n "$TRAVIS_JOB_ID" ]; then
+    echo "Generated these packs:"
+    ls -l /tmp/kui-packs/
+fi
 
 for i in /tmp/kui-packs/*.tgz; do
     mv $i ${i//-[[:digit:]].[[:digit:]].[[:digit:]]}
 done
 
 echo "$(tput setaf 2)done:$(tput sgr0) packs generated in /tmp/kui-packs"
+
+exit $CODE
