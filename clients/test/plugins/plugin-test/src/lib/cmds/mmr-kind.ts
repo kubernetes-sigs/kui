16e30d3443d15e7d0b9e7456917c81521be97cf2
Mengting Yan <mengting.yan1@ibm.com>
2019-11-04 16:25:33 -0500

    test: expand Client Test to test MMR kind
    Fixes #3142

---
+++ clients/test/plugins/plugin-test/src/lib/cmds/mmr-kind.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This file introduces a "test mmr kind" command that opens the sidecar with
+ * a plain text mode associated with `kind`.
+ *
+ */
+
+import { Commands, UI } from '@kui-shell/core'
+
+import { plainTextMode } from './content/modes'
+
+const metadata = {
+  kind: 'this is the kind part',
+  metadata: {
+    name: 'this is the name part'
+  }
+}
+
+const doModes = (): (() => UI.MultiModalResponse) => {
+  return () => Object.assign(metadata, { modes: plainTextMode })
+}
+
+export default (commandTree: Commands.Registrar) => {
+  commandTree.listen('/test/mmr/kind', doModes(), {
+    usage: {
+      docs: 'A showcase of MultiModalResponse kind'
+    }
+  })
+}
--- clients/test/plugins/plugin-test/src/test/response/mmr-kind.ts
+++ clients/test/plugins/plugin-test/src/test/response/mmr-kind.ts
@@ -14,14 +14,22 @@
  * limitations under the License.
  */
 
-import { Commands } from '@kui-shell/core'
+/**
+ * This file tests "test mmr kind" command that opens the sidecar with
+ * a plain text mode associated with `kind`.
+ *
+ * See the command implementation in: plugin-test/src/lib/cmds/mmr-kind.ts
+ *
+ */
 
-import sayHello from './lib/cmds/say-hello'
-import mmrName from './lib/cmds/mmr-name'
-import mmrNamespace from './lib/cmds/mmr-namespace'
-import mmrKind from './lib/cmds/mmr-kind'
+import { TestMMR } from '@kui-shell/test'
 
-export default async (commandTree: Commands.Registrar) => {
-  // commands
-  await Promise.all([sayHello(commandTree), mmrName(commandTree), mmrNamespace(commandTree), mmrKind(commandTree)])
-}
+const test = new TestMMR({
+  command: 'test mmr kind',
+  kind: 'this is the kind part',
+  metadata: {
+    name: 'this is the name part'
+  }
+})
+
+test.kind()
