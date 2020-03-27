8cdd10660b8d2bc7ab5fa23e9b05ffcad49f2dee
Nick Mitchell <nickm@us.ibm.com>
2020-03-27 16:11:18 -0400

    feat(plugins/plugin-kubectl): restore Show Logs button for kubectl
    Fixes #4089

---
+++ plugins/plugin-kubectl/logs/src/preload.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { isHeadless, PreloadRegistrar } from '@kui-shell/core'
+
+export default async (registrar: PreloadRegistrar) => {
+  if (!isHeadless()) {
+    const { default: drilldownToLogs } = await import('./modes/show-logs')
+
+    registrar.registerModes(drilldownToLogs)
+  }
+}
