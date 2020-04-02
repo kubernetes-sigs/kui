390cd50d8338e624ca7743aa57d6f77cc439485e
avhhh <yavhuang@gmail.com>
2020-04-02 17:59:39 -0400

    added new iter8 plugin

--- plugins/plugin-iter8/src/plugin.ts
+++ plugins/plugin-iter8/src/plugin.ts
@@ -2,9 +2,8 @@ import { Registrar } from '@kui-shell/core'
 
 // Registers a custom command
 export default async (registrar: Registrar) => {
-  registrar.listen('/create/exp', () => ({
-    kind: 'Command',
-    metadata: { name: 'Experiment Creation' },
-    modes: []
-  }))
+        registrar.listen('/create/exp', () => ({
+                kind: 'Command',
+                metadata: { name: 'Experiment Creation' },
+                modes: []}))
 }
