196ebf37ab099ae3b64cf6d0f55dd4d434b1024d
avhhh <yavhuang@gmail.com>
2020-04-10 00:51:53 -0400

    added interface for metric config

--- plugins/plugin-iter8/src/modes/get_cluster_info.ts
+++ plugins/plugin-iter8/src/modes/get_cluster_info.ts
@@ -1,13 +1,18 @@
-import {  Arguments } from '@kui-shell/core'
-import { KubeOptions } from '@kui-shell/plugin-kubectl'
+import { Tab } from '@kui-shell/core'
 
-function parseData(data: string): Array<string> {
+function parseData(data: string): Array<Object> {
 	var dataArr = data.split(" ")
-	return dataArr
+	var dict = []
+	for(let i = 0; i < dataArr.length-1; i++){
+    dict.push({
+        id: 'ns-'+i,
+        text: dataArr[i]
+    })
+	}
+	return dict
 }
 
-export async function getNsData(args: Arguments<KubeOptions>): Promise<Array<string>>{
-	const { parsedOptions: options, REPL } = args
+export async function getNsData(tab: Tab): Promise<Array<Object>>{
 	const cmd = `kubectl get ns -o=jsonpath='{range .items[*]}{.metadata.name}{" "}'`	
-	return parseData(await(REPL.qexec(cmd)))
+	return parseData(await(tab.REPL.qexec(cmd)))
 }
