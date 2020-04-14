855b768770266b6a190fdaff184b1161a7e05a59
avhhh <yavhuang@gmail.com>
2020-04-14 13:46:12 -0400

    added Decision tab & fixed file names

--- plugins/plugin-iter8/src/preload.ts
+++ plugins/plugin-iter8/src/preload.ts
@@ -1,7 +1,9 @@
 import { PreloadRegistrar } from "@kui-shell/core"
-import exprcreateMode from './models/exprSetup'
+import { exprcreateMode, decisionMode } from './models/renderCreateExpr'
+
 export default async (registrar: PreloadRegistrar) => {
 	await registrar.registerModes(
-		exprcreateMode
+		exprcreateMode,
+		decisionMode
 		)
 }
