bacfc2ec47bf47a0f1a71937779d4c813c7d9d59
avhhh <yavhuang@gmail.com>
2020-04-14 13:46:12 -0400

    added Decision tab & fixed file names

--- plugins/plugin-iter8/src/modes/decisionForm.tsx
+++ plugins/plugin-iter8/src/modes/decisionForm.tsx
@@ -2,35 +2,29 @@ import * as React from 'react'
 import { InlineLoading } from 'carbon-components-react'
 import 'carbon-components/scss/components/loading/_loading.scss'
 import '../../src/web/scss/static/decisionForm.scss'
+class DecisionBase extends React.Component<any, any> {
+	
+	public constructor(props){
+		super(props);
+		this.state = {
+			exprCreated: false //User has finished expr setup
+		}
+	}
 
-export class DecisionBase extends React.Component<any, any> {
-  public constructor(props) {
-    super(props)
-    this.state = {
-      exprCreated: false // User has finished expr setup
-    }
-  }
-
-  public render() {
-    return (
-      <div>
-        <InlineLoading
-          description={
-            !this.state.exprCreated ? 'Waiting for Experiment Creation Completion...' : 'Experiment Created.'
-          }
-          iconDescription="Active loading indicator"
-          status="active"
-          onSuccess={this.state.exprCreated}
-        />
-      </div>
-    )
-  }
+	public render(){
+		return(
+			<InlineLoading
+				description="Waiting for Experiment creation..."
+				iconDescription="Active loading indicator"
+				status="active"
+				onSuccess={this.state.exprCreated}
+			/>
+		)
+	}
 }
 
-export function renderDecisionTab() {
-  return {
-    react: function renderComponent() {
-      return <DecisionBase />
-    }
-  }
+export function renderDecisionTab(){
+	return {
+		react: () => <DecisionBase />
+	}
 }
