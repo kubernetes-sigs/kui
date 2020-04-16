4a310f1d4b13142c38dc997a53b0827fb12196eb
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2020-04-16 17:10:14 -0400

    Async call to iter8 analytics service

--- plugins/plugin-iter8/src/plugin.ts
+++ plugins/plugin-iter8/src/plugin.ts
@@ -2,8 +2,9 @@ import { Registrar } from '@kui-shell/core'
 
 // Registers a custom command
 export default async (registrar: Registrar) => {
-        registrar.listen('/create/exp', () => ({
-                kind: 'Command',
-                metadata: { name: 'Experiment Creation' },
-                modes: []}))
+  registrar.listen('/create/exp', () => ({
+    kind: 'Command',
+    metadata: { name: 'Experiment Creation' },
+    modes: []
+  }))
 }
