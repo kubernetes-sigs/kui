6dd57591571337f06e85579767c79f6e6f68bbc7
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2020-04-16 17:55:56 -0400

    minor change

--- plugins/plugin-iter8/src/plugin.ts
+++ plugins/plugin-iter8/src/plugin.ts
@@ -1,5 +1,4 @@
 import { Registrar } from '@kui-shell/core'
-
 // Registers a custom command
 export default async (registrar: Registrar) => {
         registrar.listen('iter8/create/experiment', () => ({
