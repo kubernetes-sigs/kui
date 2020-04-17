f1e08f0257156b69b09cbcc40e1ad53716aa9794
avhhh <yavhuang@gmail.com>
2020-04-17 13:02:13 -0400

    debugged the limitval

--- plugins/plugin-iter8/src/modes/decisionForm.tsx
+++ plugins/plugin-iter8/src/modes/decisionForm.tsx
@@ -7,8 +7,13 @@ const options = {
 	labels:['reviews-v1', 'reviews-v2', 'reviews-v3']
 };
 const series = [20, 55, 25];	//add to 100?
-
-class DecisionBase extends React.Component<any, any> {
+// <Chart
+// 	type="pie"
+// 	options={options}
+// 	series={series}
+// 	width="500"
+// />
+export class DecisionBase extends React.Component<any, any> {
 	public static displayName = 'DecisionBase';
 	public constructor(props){
 		super(props);
@@ -19,19 +24,14 @@ class DecisionBase extends React.Component<any, any> {
 	public render(){
 		return(
 			<div>
-				<InlineLoading
-					description={!this.state.exprCreated ?
-						 "Waiting for Experiment Creation Completion...":"Experiment Created."}
-					iconDescription="Active loading indicator"
-					status="active"
-					onSuccess={this.state.exprCreated}
-				/>
-				<Chart
-					type="pie"
-					options={options}
-					series={series}
-					width="500"
-				/>
+			<InlineLoading
+				description={!this.state.exprCreated ?
+					 "Waiting for Experiment Creation Completion...":"Experiment Created."}
+				iconDescription="Active loading indicator"
+				status="active"
+				onSuccess={this.state.exprCreated}
+			/>
+
 			</div>
 		)
 	}
