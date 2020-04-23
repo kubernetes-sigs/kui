d51bcd2bfa37a6bfe8da191211c68a7dbb727b4e
avhhh <yavhuang@gmail.com>
2020-04-23 14:24:11 -0400

    dictionary for pretty display names

---
+++ plugins/plugin-iter8/src/components/displayname-dict.ts
@@ -0,0 +1,37 @@
+// Dictionary to convert the metric config name -> Human readible
+export default class DisplayName {
+
+	// Dictionary for metrics
+	private metricDisplayNames = {
+		iter8_request_count: "Request Count",
+		iter8_total_latency: "Total Latency (msec)",
+		iter8_error_count: "Error Count",
+		iter8_mean_latency: "Mean Latency (msec)",
+		iter8_error_rate: "Error Rate"
+	}
+
+	// Dictionary for displaying Analytics Algorithms
+	private algoDisplayNames = {
+		unif: "Uniform Split"
+	}
+
+	/* 
+	* === Methods to check the dictionary for a human readable name ===
+	*/
+	public getMetricName(configName: string) :string{
+		if (configName in this.metricDisplayNames)
+			return this.metricDisplayNames[configName]
+		else{
+			console.log("Metric name is not registed in the dictionary.");
+			return configName
+		}
+	}
+	public getAlgoName(algoName: string) :string {
+		if(algoName in this.algoDisplayNames)
+			return this.algoDisplayNames[algoName]
+		else{
+			console.log("Algorithm name is not registered in the dictionary.")
+			return algoName
+		}
+	}
+}
