8aa0a8ee2d80eb99c83e5339db11ea5502b2c59a
avhhh <yavhuang@gmail.com>
2020-04-24 12:36:23 -0400

    full decision tab view

--- plugins/plugin-iter8/src/modes/decisionForm.tsx
+++ plugins/plugin-iter8/src/modes/decisionForm.tsx
@@ -1,47 +1,88 @@
 import * as React from 'react'
 import { eventChannelUnsafe } from '@kui-shell/core'
 import { Form, FormGroup, InlineLoading, Button, Slider, Dropdown, DataTable } from 'carbon-components-react'
-import { Renew32 } from '@carbon/icons-react'
+import { Renew32, Undo32, Export32 } from '@carbon/icons-react'
 import Chart from 'react-apexcharts'
 import 'carbon-components/scss/components/loading/_loading.scss'
 import 'carbon-components/scss/components/form/_form.scss'
 import 'carbon-components/scss/components/button/_button.scss'
 import 'carbon-components/scss/components/slider/_slider.scss'
 import 'carbon-components/scss/components/dropdown/_dropdown.scss'
+import 'carbon-components/scss/components/data-table/_data-table.scss'
 import '../../src/web/scss/static/decisionForm.scss'
 import { DecisionState, RequestModel } from '../components/get-iter8-req'
 import GetAnalyticsAssessment from "../components/get-analytics-assessment"
-
+import DisplayName from "../components/displayname-dict"
+//Deconstructs the DataTable component
+const util = require('util');
+const {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHeader,
+} = DataTable;
 //sample values for testing purposes
 const sampleHeaders=[
     {
-    	header: 'Version',
-    	key: 'version'
+      header: 'Deployment Version',
+      key: 'version'
     },
     {
-    	header: 'Mean Latency',
-    	key: 'mean_latency'
+      header: 'Mean Latency (msec)',
+      key: 'mean_latency'
     },
     {
-    	header: "Total Error",
-    	key: 'tot_error'
+      header: 'Total Errors',
+      key: 'tot_error'
     }
-]
+  ]
 
 const sampleRows = [
     {
-    	id: 0,
-    	version: "reviews-v2",
-    	mean_latency: 15,
-    	tot_error: 20,
+      id: 'reviews-v2',
+      version: "reviews-v2",
+      mean_latency: 20,
+      tot_error: 10
     },
     {
-    	id: 1,
-    	version: "reviews-v2",
-    	mean_latency: 15,
-    	tot_error: 20,
+      id: 'reviews-v3',
+      version: "reviews-v3",
+      mean_latency: 13,
+      tot_error: 2
     }
   ]
+// Functional Component for Data Tabel rendering
+const renderTable = ({rows, headers}) => (
+  <TableContainer 
+  	title="Metrics Comparison" 
+  	description="Current values of all experimental metrics"
+  >
+    <Table>
+      <TableHead>
+        <TableRow>
+          {headers.map(header => (
+            <TableHeader>
+              {header.header}
+            </TableHeader>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map(row => (
+          <TableRow key={row.id}>
+            {row.cells.map(cell => (
+              <TableCell key={cell.id}>{cell.value}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
+
 export class DecisionBase extends React.Component<{}, DecisionState> {
   // For displaying Pie Chart
   private winProbData = [];
@@ -49,6 +90,9 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   // For displaying Traffic Suggestion Section
   private algoList = [];
   private trafficRecs = [];
+  // For displaying Metric Comparison Section
+  private metricTableHeaders = [];
+  private metricTableRows = [];
   public constructor(props) {
     super(props)
     this.state = {
@@ -58,7 +102,7 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
       exprReq: null,
       exprResult: null,
     }
-    eventChannelUnsafe.on('/my/channel', (formstate) => { this.setState({exprCreated: true, exprReq: formstate}); });
+    eventChannelUnsafe.on('/my/channel', (formstate) => {this.setState({exprCreated: true, exprReq: formstate}); });
   }
   //Makes an AJAX call to Iter8 API
   private refreshHandler = () => {
@@ -106,15 +150,12 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   }
   // Get the list of algorithms available
   private getAlgo(){
-  	let trafficRecs = this.state.exprResult.traffic_split_recommendation;
-  	let algoName = ''
+  	let trafficRecs = this.state.exprResult.traffic_split_recommendation;  	
   	let tempAlgoList = []
+  	let DisplayDict = new DisplayName();
   	//Iterate through the traffic recommendations
   	Object.getOwnPropertyNames(trafficRecs).forEach(key => {
-	  if(key === 'unif'){
-	  	algoName = "Uniform Split"
-	  }
-	  	
+  	  let algoName = DisplayDict.getAlgoName(key);	  	
 	  tempAlgoList.push({
 	  	id: key,
 	  	text: algoName
@@ -139,6 +180,47 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   	});
   	this.trafficRecs = recList;
   }
+  // Populate the metric table header
+  private getMetricHeaders(){
+  	let DisplayDict = new DisplayName();
+  	let tempHeaders = [{header: "Deployment Name", key: "version"}];
+  	// An array returned
+  	let baseAssess = this.state.exprResult.baseline_assessment.criterion_assessments;
+  	for(let i = 0; i < baseAssess.length; i++){
+  		let metricId = baseAssess[i].metric_id;
+  		let metricName = DisplayDict.getMetricName(metricId);
+  		tempHeaders.push({
+  			header: metricName,
+  			key: metricId,
+  		});
+  	}
+  	this.metricTableHeaders = tempHeaders;
+  }
+
+  private getMetricRows(){
+	let baseCriteria = this.state.exprResult.baseline_assessment.criterion_assessments;
+	let baseId = this.state.exprResult.baseline_assessment.id;
+
+	let tempRows = [];
+	let tempBase = {"id":baseId, "version": baseId}
+	for(let i = 0; i < baseCriteria.length; i++){
+		tempBase[baseCriteria[i].metric_id] = baseCriteria[i].statistics.value;
+	}
+	tempRows.push(tempBase);
+	// Iterate through every candidate assessment
+	let candList = this.state.exprResult.candidate_assessments;
+	for(let i = 0; i < candList.length; i++){
+		let tempCand = {"id":candList[i].id, "version": candList[i].id}
+		//Iterate through every metric in the candidate
+		let candAssess = candList[i].criterion_assessments;
+		for(let j = 0; j < candAssess.length; j++){
+			tempCand[candAssess[j].metric_id] = candAssess[j].statistics.value;
+		}
+		tempRows.push(tempCand);
+	}
+	this.metricTableRows = tempRows;
+  }
+  // On changing the algorithm type
   private handleAlgoChange = (value) => {
   	this.setState({selectedAlgo: value.id});
   	this.getTrafficRecs(value.id);
@@ -147,6 +229,8 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   	if(this.state.haveResults){
   		this.getWinProbs();
   		this.getAlgo();
+  		this.getMetricHeaders();
+  		this.getMetricRows();
   		// Fills the arrays with an inital value
   		if(this.state.selectedAlgo === ''){
 			//Assumes algorithm list is not empty
@@ -175,12 +259,6 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
 	          Refresh
 	        </Button>
         </FormGroup>
-        <FormGroup legendText="Metric Summary">
-        	<DataTable
-        		headers={sampleHeaders}
-        		rows={sampleRows}
-			/>
-        </FormGroup>
         {this.state.haveResults ? (
         	<div>
         	<FormGroup legendText="">
@@ -206,6 +284,21 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
 					helperText="Choose the algorithm to view its traffic routing suggestions."
 					onChange={(value) => this.handleAlgoChange(value.selectedItem)}
 				/>
+				<Button
+					size="default"
+					kind="danger"
+					renderIcon={Undo32}
+					onClick={() => this.getTrafficRecs(this.state.selectedAlgo)}
+				>
+					Reset
+				</Button>
+				<Button
+					size="default"
+					kind="primary"
+					renderIcon={Export32}
+				>
+					Apply
+				</Button>
 				</FormGroup>
 				<FormGroup legendText=''>
 				{this.trafficRecs.map((val, idx) => {
@@ -221,8 +314,13 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
 					)
 				})}
 				</FormGroup>
-				<FormGroup legendText="Metric Summary">
-				</FormGroup>
+		        <FormGroup legendText="">
+		        	<DataTable
+		        		headers={this.metricTableHeaders}
+		        		rows={this.metricTableRows}
+		        		render={({rows, headers}) => renderTable({rows, headers})}
+					/>
+		        </FormGroup>
 			</div>
         ) : null}
       </Form>
