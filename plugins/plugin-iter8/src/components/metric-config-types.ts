d66d31424d516553dadecda98c96a7e580069acc
Alan Cha <Alan.cha1@ibm.com>
2020-05-07 10:51:27 -0400

    Add metric config types

---
+++ plugins/plugin-iter8/src/components/metric-config-types.ts
@@ -0,0 +1,40 @@
+// iter8 metric config map type definition
+
+type MetricConfigMap = {
+  apiVersion: string
+  kind: string
+  metadata: {
+    name: string
+    namespace: string
+  }
+  data: {
+    'counter_metrics.yaml': string
+    'ratio_metrics.yaml': string
+  }
+}
+
+type CounterMetrics = CounterMetric[]
+
+type CounterMetric = {
+  name: string
+  query_template: string
+  preferred_direction: preferredDirections
+  units: string
+}
+
+type RatioMetrics = RatioMetric[]
+
+type RatioMetric = {
+  name: string
+  numerator: string
+  denominator: string
+  preferred_direction: preferredDirections
+  zero_to_one?: boolean
+}
+
+enum preferredDirections {
+  'lower',
+  'higher'
+}
+
+export { MetricConfigMap, CounterMetrics, CounterMetric, RatioMetrics, RatioMetric, preferredDirections }
