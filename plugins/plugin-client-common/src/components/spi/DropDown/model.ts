c53c12fb7bc7d3584551eb8fa2f965fd8a59ff29
Mengting Yan <mengting.yan1@ibm.com>
2020-05-19 12:15:11 -0400

    feat(plugins/plugin-client-common): backport Dropdown spi
    Fixes #4635

---
+++ plugins/plugin-client-common/src/components/spi/DropDown/model.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface Action {
+  label: string
+  handler: () => void
+}
+
+interface Props {
+  /** Actions to be rendered in an overflow menu associated with the Card */
+  actions: Action[]
+
+  /** [Optional] CSS class to associate with the outermost Card element */
+  className?: string
+
+  /** [Optional] Handler called when the DropDown is closed */
+  onClose?: () => void
+}
+
+export default Props
