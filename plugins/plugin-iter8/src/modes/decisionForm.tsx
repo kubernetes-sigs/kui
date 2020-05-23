23250855eeac9ab5d0b2f21ac08ae2e7a9da5d17
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2020-05-23 00:55:41 -0400

    advanced statistics table dropdown and dummy values added

--- plugins/plugin-iter8/src/modes/decisionForm.tsx
+++ plugins/plugin-iter8/src/modes/decisionForm.tsx
@@ -12,7 +12,7 @@ import {
   InlineNotification,
   ToastNotification
 } from 'carbon-components-react'
-import { Renew32, Undo32, Export32 } from '@carbon/icons-react'
+import { Renew32, Undo32, Export32, Data_132 as Data132 } from '@carbon/icons-react'
 import Chart from 'react-apexcharts'
 // Styling imports
 import 'carbon-components/scss/components/loading/_loading.scss'
@@ -35,6 +35,7 @@ const { TableContainer, Table, TableHead, TableRow, TableBody, TableCell, TableH
 interface TableProps {
   rows: any
   headers: any
+  getHeaderProps: any
 }
 // Functional Component for Data Table rendering
 const renderTable = TableProps => (
@@ -43,7 +44,9 @@ const renderTable = TableProps => (
       <TableHead>
         <TableRow>
           {TableProps.headers.map(header => (
-            <TableHeader key={header.key}>{header.header}</TableHeader>
+            <TableHeader {...TableProps.getHeaderProps({ header })} key={header.key}>
+              {header.header}
+            </TableHeader>
           ))}
         </TableRow>
       </TableHead>
@@ -59,7 +62,99 @@ const renderTable = TableProps => (
     </Table>
   </TableContainer>
 )
-
+/*eslint-disable */
+// var finalans = {
+//   "timestamp": "2020-05-22T16:15:10.758576",
+//   "baseline_assessment": {
+//     "id": "reviews_v2",
+//     "request_count": 8,
+//     "criterion_assessments": [
+//       {
+//         "id": "0",
+//         "metric_id": "iter8_mean_latency",
+//         "statistics": {
+//           "value": 0.010749841000000001,
+//           "ratio_statistics": null
+//         },
+//         "threshold_assessment": null
+//       },
+//       {
+//         "id": "1",
+//         "metric_id": "iter8_error_rate",
+//         "statistics": {
+//           "value": 0,
+//           "ratio_statistics": null
+//         },
+//         "threshold_assessment": null
+//       },
+//       {
+//         "id": "2",
+//         "metric_id": "iter8_request_count",
+//         "statistics": {
+//           "value": 8.09915,
+//           "ratio_statistics": null
+//         },
+//         "threshold_assessment": null
+//       }
+//     ],
+//     "win_probability": 0.5
+//   },
+//   "candidate_assessments": [
+//     {
+//       "id": "reviews_v3",
+//       "request_count": 16,
+//       "criterion_assessments": [
+//         {
+//           "id": "0",
+//           "metric_id": "iter8_mean_latency",
+//           "statistics": {
+//             "value": 0.00962141154047805,
+//             "ratio_statistics": null
+//           },
+//           "threshold_assessment": null
+//         },
+//         {
+//           "id": "1",
+//           "metric_id": "iter8_error_rate",
+//           "statistics": {
+//             "value": 0,
+//             "ratio_statistics": null
+//           },
+//           "threshold_assessment": null
+//         },
+//         {
+//           "id": "2",
+//           "metric_id": "iter8_request_count",
+//           "statistics": {
+//             "value": 16.698300000000003,
+//             "ratio_statistics": null
+//           },
+//           "threshold_assessment": null
+//         }
+//       ],
+//       "win_probability": 0.5,
+//       "rollback": false
+//     }
+//   ],
+//   "traffic_split_recommendation": {
+//     "uniform": {
+//       "reviews_v3": 60,
+//       "reviews_v2": 40
+//     },
+//     "random": {
+//       "reviews_v3": 30,
+//       "reviews_v2": 70
+//     }
+//   },
+//   "winner_assessment": {
+//     "winning_version_found": false,
+//     "current_winner": null,
+//     "winning_probability": null
+//   },
+//   "status": [],
+//   "status_interpretations": {},
+//   "last_state": {}}
+/* eslint-enable */
 export class DecisionBase extends React.Component<{}, DecisionState> {
   private winner = ''
   // For displaying Pie Chart
@@ -69,13 +164,19 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   private algoList = []
   private trafficRecs = []
   // For displaying Metric Comparison Section
-  private metricTableHeaders = []
-  private metricTableRows = []
+  private criteriaTableHeaders = []
+  private criteriaTableRows = []
   private notifKey = 0
+
+  // For Displaying Advanced Statistics Section
+  private advancedStatisticsObject = {}
+  private advancedStatisticsNames = {}
+  private advancedStatiticsHeaders = []
+
   public constructor(props) {
     super(props)
     this.state = {
-      selectedAlgo: 'unif', // Assumes that unif is always the first algorithm
+      selectedAlgo: 'uniform', // Assumes that uniform is always the first algorithm
       trafficSplit: [{ version: '', split: 0 }],
       trafficErr: false, // true if sum(traffic) != 100
       notifyUser: false, // true if vs has been successfully created
@@ -83,17 +184,23 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
       experimentCreated: false, // true if user has finished expr creation
       haveResults: false, // true if Iter8 AJAX call has been successful
       experimentRequest: null, // JSON object sent as Iter8 Request
-      experimentResult: null // JSON object returned from Iter8 API
+      experimentResult: null, // JSON object returned from Iter8 API
+      haveAdvancedStatistics: false,
+      haveCriteriaComparison: false,
+      selectedAdvancedStatistic: 'Improvement Over Baseline',
+      showAdvancedStatistics: false,
+      advancedStatisticsRows: []
     }
     eventChannelUnsafe.on('/get/decision', formstate => {
-      console.log(JSON.stringify(formstate))
       this.setState({ experimentCreated: true, experimentRequest: formstate })
     })
     // Bound NON-lambda functions to component's scope
     this.handleReset = this.handleReset.bind(this)
     this.handleApply = this.handleApply.bind(this)
     this.handleCloseNotif = this.handleCloseNotif.bind(this)
-    this.handleRefresh = this.handleRefresh.bind(this)
+    this.handleGetAssessment = this.handleGetAssessment.bind(this)
+    this.toggleAdvancedStatistics = this.toggleAdvancedStatistics.bind(this)
+    this.getAdvancedStatistics = this.getAdvancedStatistics.bind(this)
   }
 
   /*
@@ -162,7 +269,7 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   }
 
   // Populate the metric table header
-  private getMetricHeaders(apiResult) {
+  private getCriteriaHeaders(apiResult) {
     const DisplayDict = new NameDict()
     const tempHeaders = [{ header: 'Deployment Name', key: 'version' }]
 
@@ -175,11 +282,11 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
         key: metricId
       })
     }
-    this.metricTableHeaders = tempHeaders
+    this.criteriaTableHeaders = tempHeaders
   }
 
   // Populate row information for metric table
-  private getMetricRows(apiResult) {
+  private getCriteriaRows(apiResult) {
     const baseCriteria = apiResult.baseline_assessment.criterion_assessments
     const baseId = apiResult.baseline_assessment.id
 
@@ -200,7 +307,119 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
       }
       tempRows.push(tempCand)
     }
-    this.metricTableRows = tempRows
+    this.criteriaTableRows = tempRows
+  }
+
+  // Returns true if the experiment involves any success/failure criteria
+  private haveCriteriaComparison() {
+    if (this.state.experimentRequest.criteria.length) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  // Returns true is the experiment involves criteria involving ratio metrics
+  private haveAdvancedStatistics() {
+    if (!this.state.experimentRequest.criteria.length) {
+      return false
+    } else {
+      this.advancedStatiticsHeaders = [{ header: 'Deployment', key: 'version' }]
+      const DisplayDict = new NameDict()
+      const ratioMetrics = this.state.experimentRequest.metric_specs.ratio_metrics
+      for (let i = 0; i < ratioMetrics.length; i++) {
+        ratioMetrics[i] = ratioMetrics[i].name
+      }
+      const criteria = this.state.experimentRequest.criteria
+      for (let i = 0; i < criteria.length; i++) {
+        if (ratioMetrics.includes(criteria[i].metric_id)) {
+          this.advancedStatiticsHeaders.push({
+            header: DisplayDict.getMetricName(criteria[i].metric_id),
+            key: criteria[i].metric_id
+          })
+        }
+      }
+      if (this.advancedStatiticsHeaders.length > 1) {
+        return true
+      }
+      return false
+    }
+  }
+
+  // Parse the advanced statistics values to be displayed in the table
+  private parseAdvancedStatisticsValues(key, val) {
+    /*eslint-disable */
+    if (key === 'credible_interval') {
+      return val['lower']
+    } else if (key === 'improvement_over_baseline') {
+      return val['lower']
+    }
+    return val
+    /* eslint-enable */
+  }
+
+  // Create a JSON object to store row values for advanced staistics
+  private createadvancedStatisticsObject() {
+    const DisplayDict = new NameDict()
+    this.advancedStatisticsNames = DisplayDict.advancedStatisticsNames
+
+    const versionRows = []
+    for (let i = 0; i < this.criteriaTableRows.length; i++) {
+      versionRows.push({ id: this.criteriaTableRows[i].id, version: this.criteriaTableRows[i].version })
+    }
+    const criterionAssessments = []
+    criterionAssessments.push(this.state.experimentResult.baseline_assessment.criterion_assessments)
+    for (let i = 0; i < this.state.experimentResult.candidate_assessments.length; i++) {
+      criterionAssessments.push(this.state.experimentResult.candidate_assessments[i].criterion_assessments)
+    }
+    const tempObject = {}
+    for (let version = 0; version < criterionAssessments.length; version++) {
+      for (let metric = 0; metric < criterionAssessments[version].length; metric++) {
+        if ({}.hasOwnProperty.call(criterionAssessments[version][metric].statistics, 'ratio_statistics')) {
+          const value = {
+            /*eslint-disable */
+            improvement_over_baseline: {
+              lower: Math.random() * -1,
+              upper: Math.random()
+            },
+            probability_of_beating_baseline: Math.random(),
+            probability_of_being_best_version: Math.random(),
+            credible_interval: {
+              lower: Math.random() * -1,
+              upper: Math.random()
+            }
+            /* eslint-enable */
+          }
+          let keys = []
+          keys = Object.values(this.advancedStatisticsNames)
+          for (let k = 0; k < keys.length; k++) {
+            if (!{}.hasOwnProperty.call(tempObject, keys[k])) {
+              tempObject[keys[k]] = JSON.parse(JSON.stringify(versionRows))
+              console.log(tempObject)
+            }
+            tempObject[keys[k]][version][
+              criterionAssessments[version][metric].metric_id
+            ] = this.parseAdvancedStatisticsValues(keys[k], value[keys[k]])
+          }
+        }
+      }
+    }
+    console.log(tempObject)
+    this.advancedStatisticsObject = tempObject
+  }
+
+  // Get and set Advanced Statistics table values
+  private getAdvancedStatistics(key) {
+    this.setState({ selectedAdvancedStatistic: key })
+    this.setState({ advancedStatisticsRows: this.advancedStatisticsObject[this.advancedStatisticsNames[key]] })
+    console.log(key)
+    console.log(this.state.advancedStatisticsRows)
+    console.log(this.advancedStatiticsHeaders)
+  }
+
+  // Toggle Show Advanced Statistics
+  private toggleAdvancedStatistics() {
+    this.setState({ showAdvancedStatistics: !this.state.showAdvancedStatistics })
   }
 
   /*
@@ -208,19 +427,30 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
    */
 
   // Makes an AJAX call to Iter8 API
-  private handleRefresh() {
+  private handleGetAssessment() {
+    this.setState({ haveResults: false })
     const AnalyticsAssess = new GetAnalyticsAssessment(this.state.experimentRequest)
     AnalyticsAssess.getAnalyticsAssessment()
       .then(result => {
         const jsonrlts = JSON.parse(JSON.parse(result))
+        // const jsonrlts = finalans
         this.getWinAnalysis(jsonrlts)
         this.getWinProbs(jsonrlts)
         this.getAlgo(jsonrlts)
-        this.getMetricHeaders(jsonrlts)
-        this.getMetricRows(jsonrlts)
+        this.getCriteriaHeaders(jsonrlts)
+        this.getCriteriaRows(jsonrlts)
         const traffic = this.getTrafficRecs(this.state.selectedAlgo, jsonrlts)
-        console.log(jsonrlts)
-        this.setState({ haveResults: true, experimentResult: jsonrlts, trafficSplit: traffic })
+        this.setState({
+          haveResults: true,
+          experimentResult: jsonrlts,
+          trafficSplit: traffic,
+          haveAdvancedStatistics: this.haveAdvancedStatistics(),
+          haveCriteriaComparison: this.haveCriteriaComparison()
+        })
+        if (this.haveAdvancedStatistics) {
+          this.createadvancedStatisticsObject()
+          this.getAdvancedStatistics(this.state.selectedAdvancedStatistic)
+        }
       })
       .catch(failure => {
         console.log(failure)
@@ -231,6 +461,7 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
   private handleAlgoChange = value => {
     this.setState({ selectedAlgo: value.id })
     this.getTrafficRecs(value.id, this.state.experimentResult)
+    this.handleReset()
   }
 
   // Handle sliders changing
@@ -278,22 +509,12 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
         <FormGroup legendText="">
           <InlineLoading
             description={
-              !this.state.experimentCreated ? 'Waiting for Experiment Creation Completion...' : 'Experiment Created.'
+              !this.state.experimentCreated ? 'Waiting for Experiment Creation Completion...' : 'Experiment Created:'
             }
             iconDescription="Active loading indicator"
             status={this.state.experimentCreated ? 'finished' : 'active'}
             style={{ width: 350 }}
           />
-          <Button
-            size="default"
-            kind="primary"
-            renderIcon={Renew32}
-            disabled={!this.state.experimentCreated}
-            onClick={this.handleRefresh}
-            className="refreshBtn"
-          >
-            Refresh
-          </Button>
         </FormGroup>
         {this.state.haveResults ? (
           <div>
@@ -366,14 +587,65 @@ export class DecisionBase extends React.Component<{}, DecisionState> {
               ) : null}
             </FormGroup>
             <FormGroup legendText="">
-              <DataTable
-                headers={this.metricTableHeaders}
-                rows={this.metricTableRows}
-                render={({ rows, headers }) => renderTable({ rows, headers })}
-              />
+              {this.state.haveCriteriaComparison ? (
+                <DataTable
+                  headers={this.criteriaTableHeaders}
+                  rows={this.criteriaTableRows}
+                  render={({ rows, headers, getHeaderProps }) => renderTable({ rows, headers, getHeaderProps })}
+                />
+              ) : (
+                <h4> No Criteria Assessment to show</h4>
+              )}
             </FormGroup>
+            <FormGroup legendText="">
+              <Button
+                style={{ position: 'relative' }}
+                size="default"
+                kind="ghost"
+                renderIcon={Data132}
+                onClick={this.toggleAdvancedStatistics}
+              >
+                Advanced Statistics
+              </Button>
+            </FormGroup>
+            {this.state.showAdvancedStatistics && this.state.haveAdvancedStatistics ? (
+              <FormGroup legendText="">
+                <Dropdown
+                  id="advancedStatistics"
+                  label="Advanced Statistics"
+                  items={Object.keys(this.advancedStatisticsNames)}
+                  initialSelectedItem={this.state.selectedAdvancedStatistic}
+                  titleText="Get Advanced Statistics"
+                  helperText="Choose the statistic to compare"
+                  onChange={value => this.getAdvancedStatistics(value.selectedItem)}
+                />
+                <DataTable
+                  headers={this.advancedStatiticsHeaders}
+                  rows={this.state.advancedStatisticsRows}
+                  render={({ rows, headers, getHeaderProps }) =>
+                    renderTable({ rows, headers, getHeaderProps, title: 'Advanced Metric Assessment' })
+                  }
+                />
+              </FormGroup>
+            ) : null}
+            {this.state.showAdvancedStatistics && !this.state.haveAdvancedStatistics ? (
+              <FormGroup legendText="">
+                <h4> Do not have Advanced Statistics</h4>
+              </FormGroup>
+            ) : null}
           </div>
         ) : null}
+        <FormGroup legendText="">
+          <Button
+            size="default"
+            kind="primary"
+            renderIcon={Renew32}
+            disabled={!this.state.experimentCreated}
+            onClick={this.handleGetAssessment}
+          >
+            Get Assessment
+          </Button>
+        </FormGroup>
       </Form>
     )
   }
