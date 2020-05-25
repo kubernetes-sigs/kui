7c6480dd40fd06d6e920c1069b921f94a92bbf36
Nick Mitchell <nickm@us.ibm.com>
2020-05-25 14:50:31 -0400

    feat(plugins/plugin-kubectl): optimize rendering of kubectl Summary tab
    Fixes #4725

---
+++ plugins/plugin-kubectl/src/lib/view/modes/Summary/impl/Generic.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as prettyPrintDuration from 'pretty-ms'
+import { REPL, Table } from '@kui-shell/core'
+
+import toMap from '../../table-to-map'
+import { KubeResource } from '../../../../model/resource'
+
+export function none() {
+  return '<none>'
+}
+
+export function age({ metadata }: KubeResource) {
+  return prettyPrintDuration(new Date().getTime() - new Date(metadata.creationTimestamp).getTime())
+}
+
+export default async function GenericSummary(resource: KubeResource, repl: REPL) {
+  // a command that will fetch a single-row table
+  const cmd = `kubectl get ${resource.kind} ${resource.metadata.name} -n ${resource.metadata.namespace} -o wide`
+  const table = repl.qexec<Table>(cmd)
+
+  return toMap(await table)
+}
