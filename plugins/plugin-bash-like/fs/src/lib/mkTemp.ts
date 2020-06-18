8e4cb7074e6dadb873dee4cbc7c85e1fe3cabeb0
Nick Mitchell <nickm@us.ibm.com>
2020-06-17 20:49:58 -0400

    fix: avoid hard-coded use of /tmp in kubectl edit
    Fixes #4928

---
+++ plugins/plugin-bash-like/fs/src/lib/mkTemp.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Arguments, RawResponse, Registrar } from '@kui-shell/core'
+
+export async function mkTemp(): Promise<RawResponse<string>> {
+  const { file: tmpFile } = await import('tmp')
+
+  return new Promise((resolve, reject) => {
+    tmpFile({ prefix: 'kui-' }, (err, tmp) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ mode: 'raw', content: tmp })
+      }
+    })
+  })
+}
+
+async function rmTemp(args: Arguments) {
+  const { unlink } = await import('fs')
+
+  return new Promise((resolve, reject) => {
+    unlink(args.argvNoOptions[1], err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
+/**
+ * Register command handlers
+ *
+ */
+export default (registrar: Registrar) => {
+  registrar.listen('/mkTemp', mkTemp, {
+    hidden: true,
+    requiresLocal: true
+  })
+
+  registrar.listen('/rmTemp', rmTemp, {
+    hidden: true,
+    requiresLocal: true
+  })
+}
