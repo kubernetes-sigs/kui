15263793c17b205efae5a029b678b5f51453d2f5
Mengting Yan <mengting.yan1@ibm.com>
2020-06-22 14:56:53 -0400

    feat: add alert command to display Alert component in Terminal
    Fixes #4957

--- plugins/plugin-client-common/src/plugin.ts
+++ plugins/plugin-client-common/src/plugin.ts
@@ -20,5 +20,6 @@ export default async (registrar: Registrar) => {
   if (!isHeadless()) {
     await import(/* webpackMode: "lazy" */ './controller/confirm').then(_ => _.default(registrar))
     await import(/* webpackMode: "lazy" */ './controller/split').then(_ => registrar.listen('/split', _.default))
+    await import(/* webpackMode: "lazy" */ './controller/alert').then(_ => _.default(registrar))
   }
 }
