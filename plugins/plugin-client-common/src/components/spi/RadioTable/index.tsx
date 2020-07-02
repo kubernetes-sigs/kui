70b8441120f75be8a5b0ea2c7e8f9b2980f24f4b
Nick Mitchell <nickm@us.ibm.com>
2020-07-02 11:20:19 -0400

    feat: MiniSplits
    Fixes #5112

--- plugins/plugin-client-common/src/components/spi/RadioTable/index.tsx
+++ plugins/plugin-client-common/src/components/spi/RadioTable/index.tsx
@@ -20,20 +20,11 @@ import Props from './model'
 import Carbon from './impl/Carbon'
 // import PatternFly4 from './impl/PatternFly'
 
-import Toolbar from '../../Content/Table/Toolbar'
-
 export interface State {
   page: number
   pageSize: number
   offset: number
   selectedIdx: number
-}
-
-/** @return the row slice for the current page */
-export function slice(props: Props & State) {
-  return !props.paginate
-    ? props.table.body
-    : props.table.body.slice((props.page - 1) * props.pageSize, props.page * props.pageSize)
 }
 
 export default class RadioTableSpi extends React.PureComponent<Props, State> {
@@ -45,27 +36,7 @@ export default class RadioTableSpi extends React.PureComponent<Props, State> {
       page: 1,
       pageSize,
       offset: 0,
-      selectedIdx: this.swapSelectedToTop(pageSize)
-    }
-  }
-
-  /**
-   * If the table is going to be paginated, bring the selected row to
-   * the top.
-   *
-   */
-  private swapSelectedToTop(pageSize: number): number {
-    if (this.isPaginated(pageSize)) {
-      const { table } = this.props
-      const selectedIdx = table.defaultSelectedIdx
-
-      const selectedRow = table.body[selectedIdx]
-      table.body[selectedIdx] = table.body[0]
-      table.body[0] = selectedRow
-
-      return 0
-    } else {
-      return this.props.table.defaultSelectedIdx
+      selectedIdx: this.props.table.defaultSelectedIdx
     }
   }
 
@@ -77,41 +48,8 @@ export default class RadioTableSpi extends React.PureComponent<Props, State> {
     return this.props.table.body.length
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  private isPaginated(pageSize = this.state.pageSize) {
-    return false
-    /**
-     * return this.props.paginate !== undefined &&
-     * this.props.paginate !== false &&
-     * (this.props.paginate === true || this.numRows() > this.props.paginate) &&
-     * this.numRows() > pageSize
-     */
-  }
-
-  private topToolbar() {
-    const { table } = this.props
-
-    const titleBreadcrumb = table.title ? [{ label: table.title, className: 'kui--data-table-title' }] : []
-    const breadcrumbs = titleBreadcrumb
-
-    return <Toolbar className="kui--data-table-toolbar-top" breadcrumbs={breadcrumbs.length > 0 && breadcrumbs} />
-  }
-
-  private bottomToolbar() {
-    return (
-      <Toolbar
-        className="kui--data-table-toolbar-bottom"
-        paginate={this.isPaginated()}
-        setPage={(page: number) => this.setState(curState => ({ page, offset: (page - 1) * curState.pageSize }))}
-        page={this.state.page}
-        totalItems={this.numRows()}
-        pageSize={this.state.pageSize}
-      />
-    )
-  }
-
   private carbon() {
-    return <Carbon {...this.props} paginate={this.isPaginated()} {...this.state} onChange={this.onChange.bind(this)} />
+    return <Carbon {...this.props} {...this.state} onChange={this.onChange.bind(this)} />
   }
 
   /* private patternfly() {
@@ -134,12 +72,6 @@ export default class RadioTableSpi extends React.PureComponent<Props, State> {
   }
 
   public render() {
-    return (
-      <div className="kui--data-table-wrapper kui--screenshotable">
-        {this.props.title && this.topToolbar()}
-        {this.broker()}
-        {this.bottomToolbar()}
-      </div>
-    )
+    return this.carbon()
   }
 }
