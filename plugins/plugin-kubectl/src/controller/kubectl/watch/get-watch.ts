0ef7fb1d39b64b70cb4c6e9ac0e4d820af72103f
Nick Mitchell <nickm@us.ibm.com>
2020-07-20 13:11:35 -0400

    fix(plugins/plugin-kubectl): events in table appear to be in random order
    only appearances, due to a time diffing bug
    
    Fixes #5212

--- plugins/plugin-kubectl/src/controller/kubectl/watch/get-watch.ts
+++ plugins/plugin-kubectl/src/controller/kubectl/watch/get-watch.ts
@@ -32,7 +32,15 @@ import {
 
 import { kindPart } from '../fqn'
 import { getKind } from '../explain'
-import { formatOf, isForAllNamespaces, getLabel, getNamespace, getResourceNamesForArgv, KubeOptions, KubeExecOptions } from '../options'
+import {
+  formatOf,
+  isForAllNamespaces,
+  getLabel,
+  getNamespace,
+  getResourceNamesForArgv,
+  KubeOptions,
+  KubeExecOptions
+} from '../options'
 
 import { getCommandFromArgs } from '../../../lib/util/util'
 import {
@@ -100,6 +108,9 @@ export class EventWatcher implements Abortable, Watcher {
   private ptyJob: Abortable[] = [] /** the pty job we spawned to capture --watch output */
   private eventLeftover: string
 
+  /** Timestamp when we started up */
+  private now = Date.now()
+
   // eslint-disable-next-line no-useless-constructor
   public constructor(
     private readonly args: Arguments,
@@ -144,14 +155,15 @@ export class EventWatcher implements Abortable, Watcher {
           })
 
         const agos = sortedRows.map(row => {
-          const ago = Date.now() - new Date(row[0].value).getTime()
+          const ts = new Date(row[0].value).getTime()
+          const ago = this.now - ts
 
           if (isNaN(ago)) {
             // kubectl displays "<unknown>"
             return strings('<unknown>')
           } else if (ago <= 0) {
             // < 0 is probably due to clock skew
-            return strings('now')
+            return strings('ago', prettyPrintDuration(0))
           } else {
             return strings('ago', prettyPrintDuration(ago >= 0 ? ago : 0, { compact: true }).toString())
           }
