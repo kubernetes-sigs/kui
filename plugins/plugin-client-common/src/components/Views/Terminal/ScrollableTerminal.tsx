d90e02b229957d4b83f982abd99b7940310060e5
Nick Mitchell <nickm@us.ibm.com>
2020-07-21 18:32:14 -0400

    fix: Editor component does not respond to tab layout changes
    Fixes #5231
    Fixes #5232

--- plugins/plugin-client-common/src/components/Views/Terminal/ScrollableTerminal.tsx
+++ plugins/plugin-client-common/src/components/Views/Terminal/ScrollableTerminal.tsx
@@ -469,6 +469,8 @@ export default class ScrollableTerminal extends React.PureComponent<Props, State
     } else if (pinBlock && this.hasReachedMaxPinnned()) {
       reject(new Error(strings('No more pins allowed')))
     } else {
+      eventBus.emitTabLayoutChange(this.props.tab.uuid)
+
       this.setState(({ splits, focusedIdx }) => {
         const newFocus = focusedIdx + 1
         const newSplits = splits
@@ -544,6 +546,8 @@ export default class ScrollableTerminal extends React.PureComponent<Props, State
    */
   private removeSplit(sbuuid: string) {
     this.setState(curState => {
+      eventBus.emitTabLayoutChange(this.props.tab.uuid)
+
       const idx = this.findSplit(this.state, sbuuid)
       if (idx >= 0) {
         curState.splits[idx].blocks.forEach(this.removeWatchableBlock)
