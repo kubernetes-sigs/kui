f5deab1c6aaf07608810d926ad142ae3bd092d30
Nick Mitchell <nickm@us.ibm.com>
2020-07-25 13:36:22 -0400

    feat: shift Block UI to use a Notebook style of presentation
    this also adds a StatusStripe widget that displays the CWD
    
    Fixes #5258

---
+++ plugins/plugin-client-common/src/components/Client/StatusStripe/CurrentWorkingDirectory.tsx
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react'
+
+import { Icons, ViewLevel, TextWithIconWidget } from '../../../'
+import { inBrowser, wireToStandardEvents, i18n } from '@kui-shell/core'
+
+const strings = i18n('plugin-client-common')
+
+interface Props {
+  className?: string
+}
+
+interface State {
+  text: string
+  viewLevel: ViewLevel
+}
+
+export default class CurrentWorkingDirectory extends React.PureComponent<Props, State> {
+  public constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      text: '',
+      viewLevel: inBrowser() ? 'hidden' : 'normal'
+    }
+  }
+
+  /**
+   * Check the current working directory
+   *
+   */
+  private async reportCurrentDirectory() {
+    const dir = inBrowser() ? process.env.PWD : process.cwd()
+    this.setState({ text: dir ? dir.replace(process.env.HOME, '~') : undefined, viewLevel: dir ? 'normal' : 'hidden' })
+  }
+
+  /**
+   * Once we have mounted, we immediately check the state,
+   * and schedule an update based on standard REPL events.
+   *
+   */
+  public componentDidMount() {
+    this.reportCurrentDirectory()
+    wireToStandardEvents(this.reportCurrentDirectory.bind(this))
+  }
+
+  public render() {
+    return (
+      <TextWithIconWidget
+        className={this.props.className}
+        text={this.state.text}
+        viewLevel={this.state.viewLevel}
+        id="kui--plugin-client-common--current-working-directory"
+        title={strings('Your current working directory')}
+        textOnclick="ls"
+      >
+        <Icons icon="Location" />
+      </TextWithIconWidget>
+    )
+  }
+}
