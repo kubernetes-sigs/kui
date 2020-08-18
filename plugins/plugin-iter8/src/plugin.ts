56310bda8c362557a32d0576dec6d97d6bf97e3e
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2020-08-17 21:17:02 -0400

    Added experimental command option + package/readme edits + test cases

--- plugins/plugin-iter8/src/plugin.ts
+++ plugins/plugin-iter8/src/plugin.ts
@@ -5,23 +5,33 @@ import { getiter8config } from './modes/config'
 
 // Registers a custom command
 export default async (registrar: Registrar) => {
-  registrar.listen('/iter8/create/experiment', () => ({
-    kind: 'Command',
-    metadata: { name: 'Experiment Creation' },
-    modes: []
-  }))
+  registrar.listen(
+    '/iter8/create/experiment',
+    () => ({
+      kind: 'Command',
+      metadata: { name: 'Experiment Creation' },
+      modes: []
+    }),
+    { isExperimental: true }
+  )
 
-  registrar.listen('/iter8/metrics', () => ({
-    kind: 'Command',
-    metadata: { name: 'Metric Command', namespace: 'Use: kubectl get configmaps -n iter8' },
-    modes: []
-  }))
+  registrar.listen(
+    '/iter8/metrics',
+    () => ({
+      kind: 'Command',
+      metadata: { name: 'Metric Command', namespace: 'Use: kubectl get configmaps -n iter8' },
+      modes: []
+    }),
+    { isExperimental: true }
+  )
 
-  const cmd = registrar.listen('/iter8/delete/metric', metricNames => metricDeleteCommand(metricNames))
+  const cmd = registrar.listen('/iter8/delete/metric', metricNames => metricDeleteCommand(metricNames), {
+    isExperimental: true
+  })
 
   registrar.synonym('/iter8/delete/metrics', metricNames => metricDeleteCommand(metricNames), cmd)
 
-  registrar.listen('/iter8/about', () => printiter8about)
+  registrar.listen('/iter8/about', () => printiter8about, { isExperimental: true })
 
-  registrar.listen('/iter8/config', () => getiter8config())
+  registrar.listen('/iter8/config', () => JSON.stringify(getiter8config()), { isExperimental: true })
 }
