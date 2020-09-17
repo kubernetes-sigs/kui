35648b87a55587d4af5324f715a8c3121b824ba9
Nick Mitchell <nickm@us.ibm.com>
2020-09-16 20:52:14 -0400

    feat(plugins/plugin-core-support): conditional opening of tabs
    Fixes #5619

---
+++ plugins/plugin-core-support/src/lib/cmds/kui-config.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Registrar } from '@kui-shell/core'
+
+const key = 'kui-shell/v1/kui-config'
+
+function load(): Record<string, string> {
+  const raw = localStorage.getItem(key)
+  return raw ? JSON.parse(raw) : {}
+}
+
+function store(settings: Record<string, string>) {
+  localStorage.setItem(key, JSON.stringify(settings))
+}
+
+function set(key: string, value: string) {
+  store(Object.assign(load(), { [key]: value }))
+}
+
+export default function(registrar: Registrar) {
+  registrar.listen('/kuiconfig/set', args => {
+    const key = args.argvNoOptions[2]
+    const value = args.argvNoOptions[3]
+
+    set(key, value)
+    return true
+  })
+
+  registrar.listen('/kuiconfig/get', args => {
+    const key = args.argvNoOptions[2]
+    return load()[key] || ''
+  })
+
+  registrar.listen('/kuiconfig/is/set', args => {
+    const key = args.argvNoOptions[3]
+    return load()[key] !== undefined
+  })
+
+  registrar.listen('/kuiconfig/not/set', args => {
+    const key = args.argvNoOptions[3]
+    return load()[key] === undefined
+  })
+}
