b7b82c5096d31485aa41603ca0623a89ae579efc
Nick Mitchell <nickm@us.ibm.com>
2020-09-28 17:06:06 -0400

    chore: Update AnimalApp example
    to kui 8.12.2
    
    Fixes #5800

--- docs/example/AnimalApp/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-
-import { Kui } from '@kui-shell/plugin-client-common'
-
-const wrapper = document.querySelector('.main')
-if (wrapper) {
-  render(<Kui />, wrapper)
-}
