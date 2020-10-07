7278d8a930dd56c6f7814f4e63dd1120351999d2
Nick Mitchell <nickm@us.ibm.com>
2020-10-07 14:18:58 -0400

    fix(plugins/plugin-core-support): notebook vfs uses win32 join when it should be using posix join
    Fixes #5903

---
+++ plugins/plugin-core-support/src/notebooks/vfs/posix.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { inBrowser } from '@kui-shell/core'
+import { basename as defaultBasename, dirname as defaultDirname, join as defaultJoin, posix } from 'path'
+
+/**
+ * This file offers a thin facade over path.posix. Webpack at least as
+ * of 4.44, does not browserify path in a way that preserves the
+ * path posix API.
+ *
+ * Luckily, its default behavior does what we need. Hence all of the
+ * inBrowser checks that dispatch to the default impl.
+ *
+ */
+
+/** @return path.basename behavior */
+export function basename(filepath: string): string {
+  return inBrowser() ? defaultBasename(filepath) : posix.basename(filepath)
+}
+
+/** @return path.dirname behavior */
+export function dirname(filepath: string): string {
+  return inBrowser() ? defaultDirname(filepath) : posix.dirname(filepath)
+}
+
+/** @return path.join behavior */
+export function join(a: string, b: string): string {
+  return inBrowser() ? defaultJoin(a, b) : posix.join(a, b)
+}
