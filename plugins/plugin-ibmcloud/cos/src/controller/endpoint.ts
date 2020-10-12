66e94b398beeb08d2d8ad9798d7d3d2bf1c91ba4
Nick Mitchell <nickm@us.ibm.com>
2020-10-12 14:12:50 -0400

    fix(plugins/plugin-ibmcloud): further refinements are needed for ibmcloud cos credentials validation
    Fixes #5962

--- plugins/plugin-ibmcloud/cos/src/controller/endpoint.ts
+++ plugins/plugin-ibmcloud/cos/src/controller/endpoint.ts
@@ -14,43 +14,94 @@
  * limitations under the License.
  */
 
-import { Arguments, eventChannelUnsafe } from '@kui-shell/core'
+import { Arguments, RadioTable, encodeComponent, eventChannelUnsafe } from '@kui-shell/core'
 
 import filepath from './filepath'
 import readConfig from './config'
 import updateChannel from '../channel'
+import { hasEndpoint } from '../model/Config'
 
 const shorthands = {
-  us: 's3.us.cloud-object-storage.appdomain.cloud',
-  'us-south': 's3.us-south.cloud-object-storage.appdomain.cloud',
-  dallas: 's3.dal.us.cloud-object-storage.appdomain.cloud',
-  'us-east': 's3.us-east.cloud-object-storage.appdomain.cloud',
-
-  eu: 's3.eu.cloud-object-storage.appdomain.cloud',
-  uk: 's3.eu-gb.cloud-object-storage.appdomain.cloud',
-  de: 's3.eu-de.cloud-object-storage.appdomain.cloud',
-
   ap: 's3.ap.cloud-object-storage.appdomain.cloud',
   au: 's3.au-syd.cloud-object-storage.appdomain.cloud',
-  jp: 's3.jp-tok.cloud-object-storage.appdomain.cloud'
+  eu: 's3.eu.cloud-object-storage.appdomain.cloud',
+  de: 's3.eu-de.cloud-object-storage.appdomain.cloud',
+  jp: 's3.jp-tok.cloud-object-storage.appdomain.cloud',
+  uk: 's3.eu-gb.cloud-object-storage.appdomain.cloud',
+  us: 's3.us.cloud-object-storage.appdomain.cloud',
+
+  'us-south': 's3.us-south.cloud-object-storage.appdomain.cloud',
+  dallas: 's3.dal.us.cloud-object-storage.appdomain.cloud',
+  'us-east': 's3.us-east.cloud-object-storage.appdomain.cloud'
 }
 
-export const usage = {
-  required: [{ name: 'endpoint', allowed: ['us', 'us-south', 'us-east', 'eu', 'uk', 'de', 'ap', 'au', 'jp'] }]
+/** @return current region; note, this value may not appear in config.json! */
+async function doGetCurrentEndpoint({ REPL }: Pick<Arguments, 'REPL'>): Promise<void | string> {
+  const raw = await REPL.qexec<string>('ibmcloud cos config region --list')
+  const match = raw.match(/Default Region\s+(.*)/)
+  if (match && match.length === 2) {
+    return match[1].trim()
+  }
 }
 
-export default async function setEndpoint(args: Arguments) {
-  const asGiven = args.argvNoOptions[args.argvNoOptions.length - 1]
-  const endpoint = shorthands[asGiven] || asGiven
-
-  const config = await readConfig(args)
+/** Talk to `ibmcloud` to set the COS endpoint to the given `endpoint` */
+async function doSetEndpoint({ REPL }: Pick<Arguments, 'REPL'>, endpoint: string) {
+  const config = await readConfig({ REPL })
   config.endpointForKui = endpoint
 
-  await args.REPL.rexec<{ data: string }>(`fwrite ${args.REPL.encodeComponent(filepath())}`, {
+  await REPL.rexec<{ data: string }>(`fwrite ${encodeComponent(filepath())}`, {
     data: JSON.stringify(config, undefined, 2)
   })
 
   eventChannelUnsafe.emit(updateChannel)
 
-  return `Set IBM Cloud Object Storage endpoint to ${endpoint}`
+  return `Setting IBM Cloud Object Storage endpoint to [${endpoint}](${endpoint})`
+}
+
+/** Present a RadioTable of endpoint options */
+async function radiotable({ REPL }: Pick<Arguments, 'REPL'>): Promise<RadioTable> {
+  const [current, local] = await Promise.all([doGetCurrentEndpoint({ REPL }), readConfig({ REPL })])
+
+  const keys = Object.keys(shorthands)
+  const defaultSelectedIdx = !current ? -1 : keys.findIndex(_ => _ === current)
+
+  if (current && !hasEndpoint(local)) {
+    await doSetEndpoint({ REPL }, current)
+  }
+
+  return {
+    apiVersion: 'kui-shell/v1',
+    kind: 'RadioTable',
+    defaultSelectedIdx,
+    title: 'Endpoint',
+    header: { cells: ['Short name', 'Endpoint'] },
+    body: keys.map(short => ({
+      cells: [short, shorthands[short]],
+      onSelect: `ibmcloud cos endpoint ${short}`
+    }))
+  }
+}
+
+export async function setEndpointIfPossible({ REPL }: Pick<Arguments, 'REPL'>): Promise<boolean> {
+  const current = await doGetCurrentEndpoint({ REPL })
+  if (current) {
+    await doSetEndpoint({ REPL }, current)
+    return true
+  } else {
+    return false
+  }
+}
+
+export default async function setEndpoint(args: Arguments) {
+  if (args.argvNoOptions.length > 4) {
+    throw new Error(
+      'Invalid usage. Try [ibmcloud cos endpoint](#kuiexec?command=ibmcloud%20cos%20endpoint) to select an endpoint'
+    )
+  } else if (args.argvNoOptions.length === 3) {
+    return radiotable(args)
+  } else {
+    const asGiven = args.argvNoOptions[args.argvNoOptions.length - 1]
+    const endpoint = shorthands[asGiven]
+    return doSetEndpoint(args, endpoint)
+  }
 }
