81b55e0a68f15f8eb2cf11147c6449b582b3bfae
Nick Mitchell <nickm@us.ibm.com>
2021-01-12 14:16:28 -0500

    fix(plugins/plugin-client-common): first-time edits of commentary may cause editor to lose focus
    Fixes #6545

--- plugins/plugin-client-common/src/components/Content/Markdown.tsx
+++ plugins/plugin-client-common/src/components/Content/Markdown.tsx
@@ -33,8 +33,7 @@ import {
   ListItem
 } from 'carbon-components-react'
 
-const CodeSnippet = React.lazy(() => import('../spi/CodeSnippet'))
-
+import CodeSnippet from '../spi/CodeSnippet'
 import 'carbon-components/scss/components/link/_link.scss'
 import '../../../web/scss/components/List/Carbon.scss'
 import '../../../web/scss/components/StructuredList/Carbon.scss'
