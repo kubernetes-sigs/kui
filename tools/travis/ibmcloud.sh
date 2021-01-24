697ede1d134b58a84d6e1f4ec48a3a69dc6a76a8
Nick Mitchell <nickm@us.ibm.com>
2021-01-23 21:12:51 -0500

    chore: remove ibmcloud plugin tests
    Fixes #6704

--- tools/travis/ibmcloud.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Installing ibmcloud CLI"
-n=0
-until [ $n -ge 20 ]; do
-    curl -L https://clis.ng.bluemix.net/install/${TRAVIS_OS_NAME} | bash - && break
-    n=$[$n+1]
-    sleep 2
-done
