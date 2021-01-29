dd02cded4a133a0ea25b739a838921eee74b23ee
Mengting Yan <mengting.yan1@ibm.com>
2021-01-29 17:23:02 -0500

    fix(plugins/plugin-kubectl): multikind table isn't unified correctly when some rows are offline
    Fixes #6865

--- plugins/plugin-kubectl/src/controller/client/direct/unify.ts
+++ plugins/plugin-kubectl/src/controller/client/direct/unify.ts
@@ -89,21 +89,21 @@ export function unifyRow(row: Row, kind: string): Row {
   const trafficLight = badgeColumnIdx >= 0 ? toTrafficLight(row.attributes[badgeColumnIdx].css) : TrafficLight.Gray
 
   const extraAttrs: Cell[] = []
-  if (row.object) {
-    const tier =
-      getFromLabel(row.object, 'tier') ||
+  const tier = row.object
+    ? getFromLabel(row.object, 'tier') ||
       getFromLabel(row.object, 'app.kubernetes.io/component') ||
       getFromSelector(row.object, 'tier')
+    : ''
 
-    extraAttrs.push({ key: 'Tier', value: tier || '' })
+  extraAttrs.push({ key: 'Tier', value: tier })
 
-    const app =
-      getFromLabel(row.object, 'app') ||
+  const app = row.object
+    ? getFromLabel(row.object, 'app') ||
       getFromLabel(row.object, 'app.kubernetes.io/name') ||
       getFromSelector(row.object, 'app')
+    : ''
 
-    extraAttrs.push({ key: 'Application', value: app || '' })
-  }
+  extraAttrs.push({ key: 'Application', value: app })
 
   return rowWith(name, kind, status, trafficLight, row, extraAttrs)
 }
