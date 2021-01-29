3733897c1fcf9347022272993f73c656224fbafd
Mengting Yan <mengting.yan1@ibm.com>
2021-01-29 16:35:12 -0500

    fix: the x in the offline badge is sometimes low contrast, especially in dark themes
    Fixes #6784

--- plugins/plugin-client-common/src/components/Content/Table/ErrorCell.tsx
+++ plugins/plugin-client-common/src/components/Content/Table/ErrorCell.tsx
@@ -20,9 +20,9 @@ export default function ErrorCell() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
       <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
-        <path fill="var(--color-base0E)" d="M0 0H24V24H0z"></path>
+        <path fill="var(--color-error-cell-bg)" d="M0 0H24V24H0z"></path>
         <path
-          fill="var(--color-base00)"
+          fill="var(--color-error-cell-text)"
           d="M15.0914286 16L12 12.9085714 8.90857143 16 8 15.0914286 11.0914286 12 8 8.90857143 8.90857143 8 12 11.0914286 15.0914286 8 16 8.90857143 12.9085714 12 16 15.0914286z"
         ></path>
       </g>
