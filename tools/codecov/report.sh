948f6fbd93dc7caa0d4d82e6070e7e394b1aeb7f
Mengting Yan <mengting.yan1@ibm.com>
2021-02-22 11:35:32 -0500

    chore: add Kubernetes Copyright headers to all files
    Fixes #7061

--- tools/codecov/report.sh
+++ tools/codecov/report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Copyright 2017-18 IBM Corporation
+# Copyright 2017-18 The Kubernetes Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
