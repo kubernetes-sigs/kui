05ac1560ca16f3b720166f33e0c9c5b43a34e5cb
Mengting Yan <mengting.yan1@ibm.com>
2021-02-24 09:19:44 -0500

    chore: update lint copyright header check to disallow year ranges
    Also add copyright headers to .yaml, .sh, Dockerfile and .conf files
    
    Fixes #7084

--- packages/builder/bin/compile.sh
+++ packages/builder/bin/compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Copyright 2017-19 The Kubernetes Authors
+# Copyright 2017 The Kubernetes Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
