b32a94063c7ed1bf46e7ea6aaafd3c48fe60eac9
Mengting Yan <mengting.yan1@ibm.com>
2021-04-16 16:37:05 -0400

    fix(plugins/plugin-client-common): disable minisplit

--- plugins/plugin-client-common/src/components/Views/Terminal/ScrollableTerminal.tsx
+++ plugins/plugin-client-common/src/components/Views/Terminal/ScrollableTerminal.tsx
@@ -1164,16 +1164,17 @@ export default class ScrollableTerminal extends React.PureComponent<Props, State
    * which indicates whether the given scrollback index is a
    * minisplit.
    *
+   * NOTE: minsplits are disable for now; see: https://github.com/kubernetes-sigs/kui/issues/7307
    */
   private readonly theseAreMiniSplits = {
     1: [false], // 1 split, not-minisplit
     2: [false, false], // 2 splits, both not-minisplit
-    3: [true, true, false], // etc.
-    4: [true, true, false, false],
-    5: [true, true, true, false, false],
-    6: [true, true, true, false, false, false],
-    7: [true, true, false, true, true, false, false],
-    8: [true, true, true, true, true, true, true, false]
+    3: [false, false, false], // etc.
+    4: [false, false, false, false],
+    5: [false, false, false, false, false],
+    6: [false, false, false, false, false, false],
+    7: [false, false, false, false, false, false, false],
+    8: [false, false, false, false, false, false, false, false]
   }
 
   /**
