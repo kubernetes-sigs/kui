1ed964c6c9f066912883040003238e2fa7ed9f3a
Mengting Yan <mengting.yan1@ibm.com>
2021-05-21 16:17:51 -0400

    fix(plugins/plugin-bash-like): on windows ls with no arguments results in Cannot read property 'replace' of undefined

--- plugins/plugin-bash-like/fs/src/vfs/delegates.ts
+++ plugins/plugin-bash-like/fs/src/vfs/delegates.ts
@@ -16,7 +16,7 @@
 
 import Debug from 'debug'
 import { basename } from 'path'
-import { Arguments, CodedError, flatten } from '@kui-shell/core'
+import { Arguments, CodedError, cwd, flatten } from '@kui-shell/core'
 import { DirEntry, VFS, absolute, findMount, multiFindMount, findMatchingMounts } from '.'
 
 const debug = Debug('plugin/bash-like/fs/vfs/delegates')
@@ -105,7 +105,7 @@ async function lsMounts(path: string): Promise<DirEntry[]> {
  *
  */
 export async function ls(...parameters: Parameters<VFS['ls']>): Promise<DirEntry[]> {
-  const filepaths = parameters[1].length === 0 ? [process.env.PWD] : parameters[1].map(absolute)
+  const filepaths = parameters[1].length === 0 ? [cwd()] : parameters[1].map(absolute)
 
   const mounts = await multiFindMount(filepaths, true)
   const vfsContentP = Promise.all(
