01253a1e556c88956ad17be11159204f7c155c89
Nick Mitchell <nickm@us.ibm.com>
2021-05-29 19:35:34 -0400

    try restoring tab completion test: plugins/plugin-core-support/src/test/core-support2/tab-completion.ts

--- plugins/plugin-core-support/src/test/core-support2/tab-completion.ts
+++ plugins/plugin-core-support/src/test/core-support2/tab-completion.ts
@@ -27,7 +27,7 @@ import { dirname, join } from 'path'
 const ROOT = dirname(require.resolve('@kui-shell/core/tests/package.json'))
 
 // temporarily, seeing if this gets us green
-xdescribe('Tab completion core', function(this: Common.ISuite) {
+describe('Tab completion core', function(this: Common.ISuite) {
   before(Common.before(this))
   after(Common.after(this))
 
