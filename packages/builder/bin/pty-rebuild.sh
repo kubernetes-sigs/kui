25fe683e01af2ab9b93d731933550242a24ff798
Nick Mitchell <nickm@us.ibm.com>
2021-05-29 16:00:25 -0400

    pty-rebuild for node

--- packages/builder/bin/pty-rebuild.sh
+++ packages/builder/bin/pty-rebuild.sh
@@ -40,15 +40,7 @@ else
         echo "rc is not a symlink"
         (cd node_modules/.bin && rm -f rc && node -e 'require("fs").symlinkSync("../rc/cli.js", "rc")')
     fi
-    if [ -e ./node_modules/.bin/prebuild-install ] && [ ! -L ./node_modules/.bin/prebuild-install ]; then
-        echo "prebuild-install is not a symlink"
-        (cd node_modules/.bin && rm -f prebuild-install && node -e 'require("fs").symlinkSync("../prebuild-install/bin.js", "prebuild-install")')
-    fi
 
     cd node_modules/node-pty
-    prebuild-install --force --download --runtime $1
+    npm run install
 fi
-
-# Notes: why --force --download?  for some reason, when run from `npm
-# run ...` prebuild-install insists on the --build-from-source option,
-# and nothing we do, short of --force, seems to override this
