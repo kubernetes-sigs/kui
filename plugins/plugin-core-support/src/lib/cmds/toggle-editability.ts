18d9f856f3a7d39790809cc325673a354f02b920
Maria Camila Ruiz Cardenas <Maria.ruiz.cardenas@ibm.com>
2021-10-11 12:04:01 -0400

    fix: Added OS menu option to toggle edit mode

--- plugins/plugin-core-support/src/lib/cmds/toggle-editability.ts
+++ plugins/plugin-core-support/src/lib/cmds/toggle-editability.ts
@@ -21,34 +21,60 @@ import {
   KResponse,
   ParsedOptions,
   Registrar,
-  StatusStripeChangeEvent
+  StatusStripeChangeEvent,
+  Tab
 } from '@kui-shell/core'
 
 interface EditOptions extends ParsedOptions {
   'new-window': boolean
   'status-stripe': StatusStripeChangeEvent['type']
+  current?: boolean
+  c?: boolean
+}
+
+/**
+ * The command usage model
+ *
+ */
+const usage = {
+  command: 'tab edit toggle',
+  strict: 'tab edit toggle',
+  optional: [{ name: '--current-tab', alias: '-c', boolean: true, docs: 'Toggle edit mode on current tab' }]
+}
+
+function getTabByIndex(argvNoOptions: string[]): Tab {
+  const index = parseInt(argvNoOptions[argvNoOptions.length - 1], 10)
+
+  if (isNaN(index)) {
+    throw new Error(`4th argument is not a number. Expected type number`)
+  } else {
+    const tmpTab = getTab(index)
+    if (tmpTab === undefined || tmpTab === null) {
+      throw new Error('Could not find tab with give index ' + index)
+    }
+    return tmpTab
+  }
 }
 
 /** Command registration */
 export default function(registrar: Registrar) {
   // register the `tab edit toggle` command
-  registrar.listen<KResponse, EditOptions>('/tab/edit/toggle', ({ argvNoOptions }) => {
-    if (argvNoOptions.length < 4) {
-      throw new Error('Not enough arguments. Expected: tab edit toggle tabIndexNum')
-    }
-
-    const index = parseInt(argvNoOptions[argvNoOptions.length - 1], 10)
-
-    if (isNaN(index)) {
-      throw new Error(`4th argument is not a number. Expected type number`)
-    } else {
-      const tab = getTab(index)
-      if (tab === undefined || tab === null) {
-        throw new Error('Could not find tab with give index ' + index)
+  registrar.listen<KResponse, EditOptions>(
+    '/tab/edit/toggle',
+    ({ tab, argvNoOptions, parsedOptions }) => {
+      if (!parsedOptions.c && argvNoOptions.length < 4) {
+        throw new Error('Not enough arguments. Expected: tab edit toggle tabIndexNum')
       }
-      const uuid = getPrimaryTabId(tab)
-      eventBus.emitWithTabId('/kui/tab/edit/toggle', uuid, tab)
+      if (argvNoOptions.length > 4) {
+        throw new Error('Too many arguments. Expected: tab edit toggle tabIndexNum')
+      }
+
+      const desiredTab = parsedOptions.c ? tab : getTabByIndex(argvNoOptions)
+
+      const uuid = getPrimaryTabId(desiredTab)
+      eventBus.emitWithTabId('/kui/tab/edit/toggle', uuid, desiredTab)
       return 'Successfully toggled edit mode'
-    }
-  })
+    },
+    { usage, flags: { alias: { 'current-tab': ['c'] }, boolean: ['c'] } }
+  )
 }
