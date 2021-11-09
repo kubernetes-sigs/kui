dfdacc5b4a2a88e0e261816890fdf0e9a1df7ad7
Nick Mitchell <nickm@us.ibm.com>
2021-11-08 20:30:59 -0500

    fix: ls in vfs directories is not normalized
    e.g. ls /kui/foo/.. should list /kui

--- plugins/plugin-bash-like/fs/src/vfs/index.ts
+++ plugins/plugin-bash-like/fs/src/vfs/index.ts
@@ -15,7 +15,7 @@
  */
 
 import slash from 'slash'
-import { isAbsolute, join } from 'path'
+import { isAbsolute, join, normalize } from 'path'
 import { Arguments, Events, ParsedOptions, REPL, Table, getCurrentTab, inBrowser, Util } from '@kui-shell/core'
 
 import { FStat } from '../lib/fstat'
@@ -210,7 +210,7 @@ export function mount(vfs: VFS | VFSProducingFunction, placeholderMountPath?: st
 
 /** @return the absolute path to `filepath` */
 export function absolute(filepath: string): string {
-  return isAbsolute(Util.expandHomeDir(filepath.toString())) ? filepath : join(Util.cwd(), filepath.toString())
+  return normalize(isAbsolute(Util.expandHomeDir(filepath.toString())) ? filepath : join(Util.cwd(), filepath))
 }
 
 /** Lookup compiatible matching mount */
