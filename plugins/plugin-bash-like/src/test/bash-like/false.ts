9f98b693258dfca2c06799799311b70fb116b91b
Nick Mitchell <nickm@us.ibm.com>
2021-12-29 14:14:18 -0500

    fix(plugins/plugin-bash-like): `false` pty executions do not exit with code 1
    in pty/client.ts, we are swallowing these, because the command has no output

---
+++ plugins/plugin-bash-like/src/test/bash-like/false.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Common, CLI, ReplExpect } from '@kui-shell/test'
+
+describe(`false command should have failed block ${process.env.MOCHA_RUN_TARGET || ''}`, function(this: Common.ISuite) {
+  before(Common.before(this))
+  after(Common.after(this))
+
+  it('should show failure for `false` command', () =>
+    CLI.command(`false`, this.app)
+      .then(ReplExpect.error(1))
+      .catch(Common.oops(this)))
+
+  it('should show failure for `true && false` command', () =>
+    CLI.command(`true && false`, this.app)
+      .then(ReplExpect.error(1))
+      .catch(Common.oops(this)))
+
+  it('should show failure for `false >& /dev/null` command', () =>
+    CLI.command(`(echo hi && false) >& /dev/null`, this.app)
+      .then(ReplExpect.error(1))
+      .catch(Common.oops(this)))
+})
