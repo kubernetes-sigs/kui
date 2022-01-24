0dcf4c39b735b2eb25b3696ea9466bb1fc68ef92
Nick Mitchell <nickm@us.ibm.com>
2022-01-24 13:04:12 -0500

    feat(plugins/plugin-client-common): allow wizards with normal kui splits

--- plugins/plugin-client-common/src/components/Content/Markdown/index.tsx
+++ plugins/plugin-client-common/src/components/Content/Markdown/index.tsx
@@ -46,7 +46,7 @@ import rehypeRaw from 'rehype-raw'
 import rehypeSlug from 'rehype-slug'
 import { kuiFrontmatter, encodePriorResponses } from './frontmatter'
 const rehypePlugins: Options['rehypePlugins'] = [tabbed, tip, wizard, codeIndexer, rehypeRaw, rehypeSlug]
-const remarkPlugins: (tab: KuiTab) => Options['remarkPlugins'] = tab => [
+const remarkPlugins: (tab: KuiTab) => Options['remarkPlugins'] = (tab: KuiTab) => [
   gfm,
   [frontmatter, ['yaml', 'toml']],
   [kuiFrontmatter, { tab }],
---
+++ plugins/plugin-client-common/tests/data/wizard-with-splits.md
@@ -0,0 +1,47 @@
+---
+layout: 
+    1: left
+    default: wizard
+---
+
+LeftSplitContent
+
+---
+
+# WizardTitleWithSplits
+
+WizardDescriptionWithSplits
+
+---
+
+## Step1Title: Step1Description
+
+Step1Body
+
+```bash
+---
+id: code-block-1
+---
+echo aaa
+```
+
+---
+
+## Step2Title: Step2Description
+
+Step2Body
+
+```bash
+---
+id: code-block-2
+---
+echo bbb
+```
+
+```bash
+---
+id: code-block-3
+---
+echo ccc
+```
+
