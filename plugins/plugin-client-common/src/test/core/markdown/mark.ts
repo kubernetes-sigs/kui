d38355a22ac2b2423dd1e0dc8cc42cfa4d0a53b1
Nick Mitchell <nickm@us.ibm.com>
2022-01-30 17:47:57 -0500

    fix(plugins/plugin-client-common): markdown mark tags sometimes confused with tabbed

--- plugins/plugin-client-common/src/test/core/markdown/mark.ts
+++ plugins/plugin-client-common/src/test/core/markdown/mark.ts
@@ -15,21 +15,39 @@
  */
 
 import { basename, dirname, join } from 'path'
-import { Common, CLI } from '@kui-shell/test'
 import { encodeComponent } from '@kui-shell/core'
+import { Common, CLI, Selectors } from '@kui-shell/test'
 
 const ROOT = join(dirname(require.resolve('@kui-shell/plugin-client-common/tests/data/marktag1.md')), '..')
 
-const IN1 = {
+interface Input {
+  input: string
+  marks: string[]
+  tabs?: string[]
+}
+
+const IN1: Input = {
   input: join(ROOT, 'data/marktag1.md'),
   marks: ['Markymark']
 }
 
-const IN2 = {
+const IN2: Input = {
   input: join(ROOT, 'data/marktag2.md'),
-  marks: ['Morkymork', 'Markymark']
+  marks: IN1.marks.concat(['Morkymork'])
 }
-;[IN1, IN2].forEach(markdown => {
+
+const IN3: Input = {
+  input: join(ROOT, 'data/marktag3.md'),
+  marks: IN2.marks,
+  tabs: ['Tab1Title', 'Tab2Title']
+}
+
+const IN4: Input = {
+  input: join(ROOT, 'data/marktag4.md'),
+  marks: IN3.marks.concat(['Mirkymirk']), // no 'Merkymerk' is it in Tab2, not yet rendered
+  tabs: IN3.tabs
+}
+;[IN1, IN2, IN3, IN4].forEach(markdown => {
   describe(`mark tags in markdown ${basename(markdown.input)} ${process.env.MOCHA_RUN_TARGET ||
     ''}`, function(this: Common.ISuite) {
     before(Common.before(this))
@@ -52,6 +70,16 @@ const IN2 = {
             })
           })
         )
+
+        if (markdown.tabs) {
+          await Promise.all(
+            markdown.tabs.map(tabTitle =>
+              this.app.client
+                .$(Selectors.Markdown.tabWithTitle(tabTitle))
+                .then(_ => _.waitForExist({ timeout: CLI.waitTimeout }))
+            )
+          )
+        }
       } catch (err) {
         await Common.oops(this, true)(err)
       }
