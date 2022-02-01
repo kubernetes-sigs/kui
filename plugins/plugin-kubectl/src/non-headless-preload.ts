70bfbf3bd596faf033a666c30703da1dce550eb6
Maria Camila Ruiz Cardenas <Maria.ruiz.cardenas@ibm.com>
2022-02-01 14:05:17 -0500

    fix(plugins/plugin-kubectl): First draft of Knative Eventing guidebook

--- plugins/plugin-kubectl/src/non-headless-preload.ts
+++ plugins/plugin-kubectl/src/non-headless-preload.ts
@@ -79,6 +79,7 @@ export default async (registrar: PreloadRegistrar) => {
       'plugin://plugin-kubectl/notebooks/knative-first-service.md',
       'plugin://plugin-kubectl/notebooks/knative-first-autoscale.md',
       'plugin://plugin-kubectl/notebooks/knative-traffic-splitting.md',
+      'plugin://plugin-kubectl/notebooks/knative-introducing-eventing.md',
       'plugin://plugin-kubectl/notebooks/create-jobs.md',
       'plugin://plugin-kubectl/notebooks/create-jobs.json',
       'plugin://plugin-kubectl/notebooks/crud-operations.md',
