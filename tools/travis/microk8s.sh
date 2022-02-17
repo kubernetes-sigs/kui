826f03b73e713b37576e106b0b4a3dd7761b632a
Nick Mitchell <nickm@us.ibm.com>
2022-02-17 14:52:57 -0500

    test: increase retry count when downloading github action CLIs
    > Could not resolve host: mirror.openshift.com

--- tools/travis/microk8s.sh
+++ tools/travis/microk8s.sh
@@ -31,7 +31,7 @@ pushd /tmp
   if [ -n "$NEEDS_HELM" ]; then
       PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
       echo "Downloading this helm: https://get.helm.sh/helm-v${TRAVIS_HELM_VERSION}-${PLATFORM}-amd64.tar.gz"
-      curl --retry 5 -L "https://get.helm.sh/helm-v${TRAVIS_HELM_VERSION}-${PLATFORM}-amd64.tar.gz" | tar zxf -
+      curl --retry 10 -L "https://get.helm.sh/helm-v${TRAVIS_HELM_VERSION}-${PLATFORM}-amd64.tar.gz" | tar zxf -
       sudo cp ${PLATFORM}-amd64/helm /usr/local/bin
       sudo chmod +x /usr/local/bin/helm
   fi
@@ -40,13 +40,13 @@ pushd /tmp
   if [ -n "$NEEDS_OC" ]; then
       PLATFORM=`uname | tr '[:upper:]' '[:lower:]'`
       echo "Downloading this oc: https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${TRAVIS_OC_VERISON}/openshift-client-${PLATFORM}.tar.gz"
-      curl --retry 5 -L "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${TRAVIS_OC_VERISON}/openshift-client-${PLATFORM}.tar.gz" | tar zxf -
+      curl --retry 10 -L "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${TRAVIS_OC_VERISON}/openshift-client-${PLATFORM}.tar.gz" | tar zxf -
       sudo cp oc /usr/local/bin
       sudo chmod +x /usr/local/bin/oc
       oc version
 
       echo "Downloading this odo: https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-${PLATFORM}-amd64"
-      sudo sh -c "curl --retry 5 -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-${PLATFORM}-amd64 -o /usr/local/bin/odo"
+      sudo sh -c "curl --retry 10 -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-${PLATFORM}-amd64 -o /usr/local/bin/odo"
       sudo chmod +x /usr/local/bin/odo
       odo version
   fi
@@ -70,14 +70,14 @@ kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-adm
 
 # Download and install kubectl (microk8s has its own kubectl, make sure we override it with the version we want)
 echo "Downloading this kubectl: https://storage.googleapis.com/kubernetes-release/release/v${TRAVIS_KUBE_VERSION}/bin/linux/amd64/kubectl"
-  curl --retry 5 -LO https://storage.googleapis.com/kubernetes-release/release/v${TRAVIS_KUBE_VERSION}/bin/linux/amd64/kubectl && \
+  curl --retry 10 -LO https://storage.googleapis.com/kubernetes-release/release/v${TRAVIS_KUBE_VERSION}/bin/linux/amd64/kubectl && \
        sudo cp kubectl /usr/local/bin/kubectl && \
        sudo chmod a+rx /usr/local/bin/kubectl
 
 # Wait for `default` serviceaccount to be ready
 # See https://github.com/kubernetes-sigs/kui/issues/7679
 echo "Waiting for default serviceaccount to be ready"
-n=0; until ((n >= 60)); do kubectl -n default get serviceaccount default -o name && break; n=$((n + 1)); sleep 1; done; ((n < 60))
+n=0; until ((n >= 90)); do kubectl -n default get serviceaccount default -o name && break; n=$((n + 1)); sleep 1; done; ((n < 90))
 echo "Waiting for default serviceaccount to be ready: DONE"
 
 echo "microk8s setup script done"
