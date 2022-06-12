886d7bb37ecb663d61d56f82eeb6e7fa561226fd
Nick Mitchell <nickm@us.ibm.com>
2022-06-12 08:34:08 -0400

    chore: bump patternfly dependencies 4.221.3

--- plugins/plugin-core-support/src/test/core-standalone/text-search.ts
+++ plugins/plugin-core-support/src/test/core-standalone/text-search.ts
@@ -16,6 +16,14 @@
 
 import { Common, CLI, Keys, ReplExpect, Selectors } from '@kui-shell/test'
 
+const searchBarSelector = '#search-bar'
+const inputSelector = `${searchBarSelector} input`
+const buttonsSelector = `${searchBarSelector} .pf-c-text-input-group__utilities`
+const countSelector = `${buttonsSelector} .pf-c-badge.pf-m-read`
+const previousButton = `${buttonsSelector} button[aria-label="Previous"]`
+const nextButton = `${buttonsSelector} button[aria-label="Next"]`
+const closeButtonSelector = `${buttonsSelector} button[aria-label="Reset"]`
+
 Common.localDescribe('Text search', function(this: Common.ISuite) {
   before(Common.before(this))
   after(Common.after(this))
@@ -41,11 +49,11 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   */
   it('should open the search bar when cmd+f is pressed', async () => {
     await this.app.client.keys([Keys.ctrlOrMeta, 'F'])
-    await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
+    await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
 
     await this.app.client.waitUntil(
       async () => {
-        return this.app.client.$('#search-bar input').then(_ => _.isFocused())
+        return this.app.client.$(inputSelector).then(_ => _.isFocused())
       },
       { timeout: CLI.waitTimeout }
     )
@@ -54,14 +62,14 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   it('should not close the search bar if pressing esc outside of search bar', async () => {
     await this.app.client.$(Selectors.CURRENT_PROMPT_BLOCK).then(_ => _.click())
     await this.app.client.keys(Keys.ESCAPE)
-    await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: 3000 }))
+    await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: 3000 }))
   })
 
   it('should focus on search bar when search bar is pressed', async () => {
-    await this.app.client.$('#search-bar').then(_ => _.click())
+    await this.app.client.$(searchBarSelector).then(_ => _.click())
     await this.app.client.waitUntil(
       async () => {
-        return await this.app.client.$('#search-bar input').then(_ => _.isFocused())
+        return await this.app.client.$(inputSelector).then(_ => _.isFocused())
       },
       { timeout: 3000 }
     )
@@ -69,7 +77,7 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
 
   it('should close the search bar via ctrl+f', async () => {
     await this.app.client.keys([Keys.ctrlOrMeta, 'F'])
-    await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
+    await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
   })
 
   /*
@@ -80,13 +88,13 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   */
   const type = async (text: string) => {
     // deleting any existing text in search bar input field
-    await this.app.client.$('#search-bar input').then(_ => _.setValue(''))
+    await this.app.client.$(inputSelector).then(_ => _.setValue(''))
     // pasting the input text into the search bar input field
-    await this.app.client.$('#search-bar input').then(_ => _.setValue(text))
+    await this.app.client.$(inputSelector).then(_ => _.setValue(text))
     // making sure the word in the input field is the same word we want to search for
     await this.app.client.waitUntil(
       async () => {
-        const actualText = await this.app.client.$('#search-bar input').then(_ => _.getValue())
+        const actualText = await this.app.client.$(inputSelector).then(_ => _.getValue())
         return actualText === text
       },
       { timeout: 3000 }
@@ -100,8 +108,8 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
         await this.app.client.waitUntil(
           async () => {
             await this.app.client.keys([Keys.ctrlOrMeta, 'F'])
-            await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: 3000 }))
-            return this.app.client.$('#search-bar input').then(_ => _.isFocused())
+            await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: 3000 }))
+            return this.app.client.$(inputSelector).then(_ => _.isFocused())
           },
           { timeout: CLI.waitTimeout }
         )
@@ -110,12 +118,10 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
         // finding number of matches
         await this.app.client.waitUntil(
           async () => {
-            await this.app.client.$('#search-bar input').then(_ => _.waitForExist({ timeout: 3000 }))
-            const txt = await this.app.client.$('#search-bar').then(_ => _.getText())
+            await this.app.client.$(inputSelector).then(_ => _.waitForExist({ timeout: 3000 }))
+            const txt = await this.app.client.$(countSelector).then(_ => _.getText())
             const totalMatches = txt.substring(txt.indexOf('/') + 1)
-            await this.app.client
-              .$('#search-bar .pf-c-search-input__clear .pf-c-button.pf-m-plain')
-              .then(_ => _.click())
+            await this.app.client.$(closeButtonSelector).then(_ => _.click())
             return totalMatches === searchFoundText
           },
           { timeout: 3000 }
@@ -144,10 +150,10 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   it('should paste into the text search box', async () => {
     // open the search bar and focus it
     await this.app.client.keys([Keys.ctrlOrMeta, 'F'])
-    await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
+    await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
     await this.app.client.waitUntil(
       async () => {
-        return this.app.client.$('#search-bar input').then(_ => _.isFocused())
+        return this.app.client.$(inputSelector).then(_ => _.isFocused())
       },
       { timeout: CLI.waitTimeout }
     )
@@ -156,7 +162,7 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
     await this.app.client.execute(() => document.execCommand('paste'))
 
     // get text from the search bar
-    const actualText = await this.app.client.$('#search-bar input').then(_ => _.getValue())
+    const actualText = await this.app.client.$(inputSelector).then(_ => _.getValue())
     return actualText === 'grumble'
   })
 
@@ -167,37 +173,29 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   */
   it('should not display results counter when search bar is opened and input field is empty', async () => {
     // closing search bar and clearing text from input field
-    await this.app.client.$('#search-bar .pf-c-search-input__clear .pf-c-button.pf-m-plain').then(_ => _.click())
+    await this.app.client.$(closeButtonSelector).then(_ => _.click())
     // open the search bar
     await this.app.client.keys([Keys.ctrlOrMeta, 'F'])
-    await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
+    await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
     await this.app.client.waitUntil(
       async () => {
-        return this.app.client.$('#search-bar input').then(_ => _.isFocused())
+        return this.app.client.$(inputSelector).then(_ => _.isFocused())
       },
       { timeout: CLI.waitTimeout }
     )
     // make sure results counter is not displayed
-    await this.app.client
-      .$('#search-bar .pf-c-search-input__count')
-      .then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
+    await this.app.client.$(countSelector).then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
   })
 
   it('should not display navigation arrows when search bar is opened and input field is empty', async () => {
     // make sure navigation arrows are not displayed
-    await this.app.client
-      .$('#search-bar button:nth-child(1)')
-      .then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
-    await this.app.client
-      .$('#search-bar button:nth-child(2)')
-      .then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
+    await this.app.client.$(previousButton).then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
+    await this.app.client.$(nextButton).then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
   })
 
   it('should not display close button when search bar is opened and input field is empty', async () => {
     // make sure close button is not displayed
-    await this.app.client
-      .$('#search-bar .pf-c-search-input__clear .pf-c-button.pf-m-plain')
-      .then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
+    await this.app.client.$(closeButtonSelector).then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
   })
 
   it('should add text to CLI, then focus on search bar and add text to input field', async () => {
@@ -206,10 +204,10 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
       .then(ReplExpect.error(127))
       .catch(Common.oops(this, true))
     // clicking on search bar to focus it
-    await this.app.client.$('#search-bar').then(_ => _.click())
+    await this.app.client.$(searchBarSelector).then(_ => _.click())
     await this.app.client.waitUntil(
       async () => {
-        return this.app.client.$('#search-bar input').then(_ => _.isFocused())
+        return this.app.client.$(inputSelector).then(_ => _.isFocused())
       },
       { timeout: 7000 }
     )
@@ -219,20 +217,18 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
 
   it('should display results counter when search bar input field has text', async () => {
     // make sure results counter is displayed
-    await this.app.client.$('#search-bar .pf-c-search-input__count').then(_ => _.waitForDisplayed({ timeout: 3000 }))
+    await this.app.client.$(countSelector).then(_ => _.waitForDisplayed({ timeout: 3000 }))
   })
 
   it('should display navigation arrows when search bar input field has text', async () => {
     // make sure navigation arrows are displayed
-    await this.app.client.$('#search-bar button:nth-child(1)').then(_ => _.waitForDisplayed({ timeout: 3000 }))
-    await this.app.client.$('#search-bar button:nth-child(2)').then(_ => _.waitForDisplayed({ timeout: 3000 }))
+    await this.app.client.$(previousButton).then(_ => _.waitForDisplayed({ timeout: 3000 }))
+    await this.app.client.$(nextButton).then(_ => _.waitForDisplayed({ timeout: 3000 }))
   })
 
   it('should display close button when search bar input field has text', async () => {
     // make sure close button is is displayed
-    await this.app.client
-      .$('#search-bar .pf-c-search-input__clear .pf-c-button.pf-m-plain')
-      .then(_ => _.waitForDisplayed({ timeout: 3000 }))
+    await this.app.client.$(closeButtonSelector).then(_ => _.waitForDisplayed({ timeout: 3000 }))
   })
 
   /*
@@ -242,12 +238,12 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   */
   it('should increase result count when "NEXT" navigation arrow is pressed', async () => {
     // getting old result count
-    const resultCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const resultCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const oldResult = resultCount.substring(0, resultCount.indexOf('/'))
     // clicking the NEXT arrow once
-    await this.app.client.$('#search-bar button:nth-child(2)').then(_ => _.click())
+    await this.app.client.$(nextButton).then(_ => _.click())
     // getting the new result count
-    const newCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const newCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const curResultCount = newCount.substring(0, newCount.indexOf('/'))
     // checking that the result count increased by one
     const oldResultCount = parseInt(oldResult) + 1
@@ -256,12 +252,12 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
 
   it('should decrease result count when "PREVIOUS" navigation arrow is pressed', async () => {
     // getting old result count
-    const resultCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const resultCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const oldResult = resultCount.substring(0, resultCount.indexOf('/'))
     // clicking the PREVIOUS arrow once
-    await this.app.client.$('#search-bar button:nth-child(1)').then(_ => _.click())
+    await this.app.client.$(previousButton).then(_ => _.click())
     // getting the new result count
-    const newCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const newCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const curResultCount = newCount.substring(0, newCount.indexOf('/'))
     // checking that the result count decreased by one
     const oldResultCount = parseInt(oldResult) - 1
@@ -270,7 +266,7 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
 
   it('should do nothing if on last result and "NEXT" navigation arrow is pressed', async () => {
     // getting current result count and total matches
-    const resultCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const resultCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const curResult = parseInt(resultCount.substring(0, resultCount.indexOf('/')))
     const totalMatches = parseInt(resultCount.substring(resultCount.indexOf('/') + 1))
 
@@ -278,16 +274,16 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
     let iter = curResult
     while (iter !== totalMatches) {
       // clicking the NEXT arrow as many times as it takes to get to last result
-      await this.app.client.$('#search-bar button:nth-child(2)').then(_ => _.click())
-      const tmp = await this.app.client.$('#search-bar').then(_ => _.getText())
+      await this.app.client.$(nextButton).then(_ => _.click())
+      const tmp = await this.app.client.$(countSelector).then(_ => _.getText())
       iter = parseInt(tmp.substring(0, tmp.indexOf('/')))
     }
 
     // try clicking the NEXT arrow once
-    await this.app.client.$('#search-bar button:nth-child(2)').then(_ => _.click())
+    await this.app.client.$(nextButton).then(_ => _.click())
 
     // getting the current result after clicking the arrow
-    const newCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const newCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const newResult = parseInt(newCount.substring(0, newCount.indexOf('/')))
 
     // checking that the result count has not changed
@@ -296,23 +292,23 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
 
   it('should do nothing if on first result and "PREVIOUS" navigation arrow is pressed', async () => {
     // getting current result count
-    const resultCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const resultCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const curResult = parseInt(resultCount.substring(0, resultCount.indexOf('/')))
 
     // navigating to the first result
     let iter = curResult
     while (iter !== 1) {
       // clicking the PREVIOUS arrow as many times as it takes to get to last result
-      await this.app.client.$('#search-bar button:nth-child(1)').then(_ => _.click())
-      const tmp = await this.app.client.$('#search-bar').then(_ => _.getText())
+      await this.app.client.$(previousButton).then(_ => _.click())
+      const tmp = await this.app.client.$(countSelector).then(_ => _.getText())
       iter = parseInt(tmp.substring(0, tmp.indexOf('/')))
     }
 
     // try clicking the PREVIOUS arrow once
-    await this.app.client.$('#search-bar button:nth-child(1)').then(_ => _.click())
+    await this.app.client.$(previousButton).then(_ => _.click())
 
     // getting the current result after clicking the arrow
-    const newCount = await this.app.client.$('#search-bar').then(_ => _.getText())
+    const newCount = await this.app.client.$(countSelector).then(_ => _.getText())
     const newResult = parseInt(newCount.substring(0, newCount.indexOf('/')))
 
     // checking that the result has not changed
@@ -325,23 +321,23 @@ Common.localDescribe('Text search', function(this: Common.ISuite) {
   ####################################################################################
   */
   it('should close the search bar if clicking the close button', async () => {
-    await this.app.client.$('#search-bar .pf-c-search-input__clear .pf-c-button.pf-m-plain').then(_ => _.click())
+    await this.app.client.$(closeButtonSelector).then(_ => _.click())
     await this.app.client.waitUntil(async () => {
       // checking that search bar isn't displayed
       const displayResults = await this.app.client
-        .$('#search-bar')
+        .$(searchBarSelector)
         .then(_ => _.waitForDisplayed({ timeout: 3000, reverse: true }))
       // open the search bar and focus it
       await this.app.client.keys([Keys.ctrlOrMeta, 'F'])
-      await this.app.client.$('#search-bar').then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
+      await this.app.client.$(searchBarSelector).then(_ => _.waitForDisplayed({ timeout: CLI.waitTimeout }))
       await this.app.client.waitUntil(
         async () => {
-          return this.app.client.$('#search-bar input').then(_ => _.isFocused())
+          return this.app.client.$(inputSelector).then(_ => _.isFocused())
         },
         { timeout: CLI.waitTimeout }
       )
       // checking that there's no text in the input field after it's been closed
-      const textInSearchBar = await this.app.client.$('#search-bar input').then(_ => _.getValue())
+      const textInSearchBar = await this.app.client.$(inputSelector).then(_ => _.getValue())
       return textInSearchBar === '' && !!displayResults
     })
   })
