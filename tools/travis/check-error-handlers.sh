57470855b8011568a34be959659281e71264fcb8
Nick Mitchell <nickm@us.ibm.com>
2023-01-10 10:37:05 -0500

    test: eliminate check-error-handlers.sh false positive

--- tools/travis/check-error-handlers.sh
+++ tools/travis/check-error-handlers.sh
@@ -17,7 +17,7 @@
 #
 
 # see https://github.com/IBM/kui/issues/5608
-if grep --include '*.ts' --color=never -r Common.oops plugins/*/src/test | grep --color=never await  | grep --color=never -v err ; then
+if grep --include '*.ts' --color=never -r Common.oops plugins/*/src/test | grep --color=never await  | grep --color=never -v err | grep --color=never -v '.catch(Common.oops' ; then
     echo "BAD TESTS: Some test error handlers will not fail"
     exit 1
 else
