a4864f6a78ce3a96b7cc387887982426281bae0d
Nick Mitchell <nickm@us.ibm.com>
2023-01-29 15:47:23 -0500

    fix: improve layout on phones

--- packages/core/src/api/Client.ts
+++ packages/core/src/api/Client.ts
@@ -138,7 +138,7 @@ export function clientGuidebooks(): string[] {
 /** @return whether to show the Sidebar onload */
 export function showGuidebooksSidebar(): boolean {
   try {
-    return require('@kui-shell/client/config.d/client.json').showGuidebooksSidebar
+    return window.innerWidth >= 800 && require('@kui-shell/client/config.d/client.json').showGuidebooksSidebar
   } catch (err) {
     return false
   }
